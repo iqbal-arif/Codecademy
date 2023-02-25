7a95523c9e717112869e7f1cf1e149e536a2c4e9
Arif Iqbal <arif_iqbal@yahoo.com>
2023-02-24 18:02:12 -0600

    new :JavaScript/Projects/Kelvin Weather/kelvin.js

---
+++ JavaScript/Projects/Kelvin Weather/kelvin.js
@@ -0,0 +1,18 @@
+// Today's Forecast in Kelvin
+
+const kelvin = 293;
+
+// Converting Kelvin to Celsius
+
+let celsius;
+celsius = kelvin - 273;
+
+// Converting Celsius to Fahrenheit
+
+let fahrenheit = celsius * (9 / 5) + 32;
+
+// Rounding down the decimal number
+
+temperature = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${temperature} degree Fahrenheit`);
