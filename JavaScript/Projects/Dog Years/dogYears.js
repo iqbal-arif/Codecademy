4f11d11eeec77104326c391ee9b7edb5b2a6ff66
Arif Iqbal <arif_iqbal@yahoo.com>
2023-02-24 19:20:24 -0600

    new :  JavaScript/Projects/Dog Years/dogYears.js modified:   JavaScript/Projects/Kelvin Weather/kelvin.js

---
+++ JavaScript/Projects/Dog Years/dogYears.js
@@ -0,0 +1,36 @@
+// Human To Dog Years Convertor
+
+// My Age
+const myAge = 47;
+
+// Early Years
+let earlyYears = 2;
+
+// Reassigning the variables
+// earlyYears = earlyYears * 10.5;
+earlyYears *= 10.5;
+
+// Accounting for First Two Years
+let laterYears = myAge - 2;
+
+// Number of Dog Years
+// laterYears = laterYears * 4;
+laterYears *= 4;
+
+// Logging Early Years and Later Years
+console.log(earlyYears, laterYears);
+
+// Adding Early Years and Later Years
+const myAgeInDogYears = earlyYears + laterYears;
+
+// Defining Name
+
+const fullName = 'Arif Iqbal';
+
+// Converting to LowerCase
+const myName = fullName.toLowerCase();
+
+// Logging the Result
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+);
--- JavaScript/Projects/Kelvin Weather/kelvin.js
+++ JavaScript/Projects/Kelvin Weather/kelvin.js
@@ -1,6 +1,6 @@
 // Today's Forecast in Kelvin
 
-const kelvin = 293;
+const kelvin = 0;
 
 // Converting Kelvin to Celsius
 
@@ -16,3 +16,13 @@ let fahrenheit = celsius * (9 / 5) + 32;
 temperature = Math.floor(fahrenheit);
 
 console.log(`The temperature is ${temperature} degree Fahrenheit`);
+
+// Converting Celsius to Newton
+
+let newton = celsius * (33 / 100);
+
+// Rounding down the decimal number
+
+let newtonTemp = Math.floor(newton);
+
+console.log(`The Newton Temperature is ${newton} `);
