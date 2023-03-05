f8647bcc482fbe2657e6a81c29df1f51ea20c96d
Arif Iqbal <arif_iqbal@yahoo.com>
2023-03-04 19:15:21 -0600

    modified:  Back_End_Engineer/Notes/JS_functions.js

--- Back_End_Engineer/Notes/JS_functions.js
+++ Back_End_Engineer/Notes/JS_functions.js
@@ -6,10 +6,10 @@ function greetInSpanish() {
   console.log('Buenas tardes.');
 }
 
-function sayThanks() {
-  console.log('Thank you for your purchase! We appreciate your business.');
+function sayThanks(name) {
+  console.log(
+    'Thank you for your purchase ' + name + '! We appreciate your business.'
+  );
 }
 
-sayThanks();
-sayThanks();
-sayThanks();
+sayThanks('Cole');
