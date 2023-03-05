91ab0ef330e8469726c2812a799eea3a99676a7a
Arif Iqbal <arif_iqbal@yahoo.com>
2023-03-04 19:35:15 -0600

    modified:  Back_End_Engineer/Notes/JS_functions.js

--- Back_End_Engineer/Notes/JS_functions.js
+++ Back_End_Engineer/Notes/JS_functions.js
@@ -22,3 +22,40 @@ function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs') {
   console.log(`Remember to buy ${item2}`);
   console.log(`Remember to buy ${item3}`);
 }
+
+// Function with Return Keyword
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+const numOfMonitors = monitorCount(5, 4);
+
+console.log(numOfMonitors);
+
+//   Helper Function
+
+// Example I:
+function multiplyByNineFifths(number) {
+  return number * (9 / 5);
+}
+
+function getFahrenheit(celsius) {
+  return multiplyByNineFifths(celsius) + 32;
+}
+
+const tempFarenheit = getFahrenheit(15); // Returns 59
+console.log(tempFarenheit);
+
+// Example II:
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+function costOfMonitors(rows, columns) {
+  return monitorCount(rows, columns) * 200;
+}
+
+const totalCost = costOfMonitors(5, 4);
+
+console.log(totalCost);
