298d86f96c9e951c9e698a72515708daf26984c0
Arif Iqbal <arif_iqbal@yahoo.com>
2023-03-04 19:17:51 -0600

    modified:  Back_End_Engineer/Notes/JS_functions.j

--- Back_End_Engineer/Notes/JS_functions.js
+++ Back_End_Engineer/Notes/JS_functions.js
@@ -2,10 +2,12 @@ function getReminder() {
   console.log('Water the plants.');
 }
 
+// Function Declaration
 function greetInSpanish() {
   console.log('Buenas tardes.');
 }
 
+// Function with Parameter
 function sayThanks(name) {
   console.log(
     'Thank you for your purchase ' + name + '! We appreciate your business.'
@@ -13,3 +15,10 @@ function sayThanks(name) {
 }
 
 sayThanks('Cole');
+
+// Default Parameter
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs') {
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
