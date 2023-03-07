eaab889a30942d542d71141db498f7b627fda8b2
Arif Iqbal <arif_iqbal@yahoo.com>
2023-03-07 10:43:09 -0600

    new file:   JavaScript/Notes/JS_blocks_scope.js

---
+++ JavaScript/Notes/JS_blocks_scope.js
@@ -0,0 +1,15 @@
+// Block Scope
+const city = 'New York City';
+
+function logCitySkyline() {
+  let skyscraper = 'Empire State Building';
+  return 'The stars over the ' + skyscraper + ' in ' + city;
+}
+
+console.log(logCitySkyline());
+
+// Global Scope
+
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
