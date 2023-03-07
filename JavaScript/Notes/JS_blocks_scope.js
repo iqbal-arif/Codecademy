29effc1404c83afb3001f253b86a86d869c4e286
Arif Iqbal <arif_iqbal@yahoo.com>
2023-03-07 11:24:36 -0600

    modified:   JavaScript/Notes/JS_blocks_scope.js

--- JavaScript/Notes/JS_blocks_scope.js
+++ JavaScript/Notes/JS_blocks_scope.js
@@ -28,3 +28,37 @@ function logVisibleLightWaves() {
 
 logVisibleLightWaves();
 console.log(lightWaves);
+
+// Scope Pollution
+
+const satellite2 = 'The Moon';
+const galaxy2 = 'The Milky Way';
+const stars2 = 'North Star';
+
+function callMyNightSky2() {
+  stars2 = 'Sirius';
+  return 'Night Sky: ' + satellite2 + ', ' + stars2 + ', and ' + galaxy2;
+}
+
+console.log(callMyNightSky2());
+console.log(stars2);
+
+/***********
+ * 
+ * Review: Scope
+
+In this lesson, you learned about scope and how it impacts the accessibility of different variables.
+
+Let’s review the following terms:
+
+    1. Scope refers to where variables can be accessed throughout the program, and is determined by where and how they are declared.
+    2. Blocks are statements that exist within curly braces {}.
+    3. Global scope refers to the context within which variables are accessible to every part of the program.
+    4. Global variables are variables that exist within global scope.
+    5. Block scope refers to the context within which variables are accessible only within the block they are defined.
+    6. Local variables are variables that exist within block scope.
+    7. Global namespace is the space in our code that contains globally scoped information.
+    8. Scope pollution is when too many variables exist in a namespace or variable names are reused.
+
+ * 
+ */
