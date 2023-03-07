960233351ab33ad2849c524ece8f89a2553f77a8
Arif Iqbal <arif_iqbal@yahoo.com>
2023-03-07 10:45:59 -0600

    modified:   JavaScript/Notes/JS_blocks_scope.js

--- JavaScript/Notes/JS_blocks_scope.js
+++ JavaScript/Notes/JS_blocks_scope.js
@@ -13,3 +13,9 @@ console.log(logCitySkyline());
 const satellite = 'The Moon';
 const galaxy = 'The Milky Way';
 const stars = 'North Star';
+
+function callMyNightSky() {
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+
+console.log(callMyNightSky());
