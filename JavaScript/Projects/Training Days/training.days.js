f3df3cc42cc46d842ffdb0ad0cae28f763948b88
Arif Iqbal <arif_iqbal@yahoo.com>
2023-03-08 09:07:24 -0600

    modified:/Projects/Training Days/training.days.js

--- JavaScript/Projects/Training Days/training.days.js
+++ JavaScript/Projects/Training Days/training.days.js
@@ -26,13 +26,13 @@ const getTrainingDays = (event) => {
 };
 
 // The scope of `name` is too tight
-const logEvent = (event) => {
-  const name = 'Nala';
+const name = 'Nala';
+
+const logEvent = (name) => {
   console.log(`${name}'s event is: ${event}`);
 };
 
-const logTime = (days) => {
-  const name = 'Nala';
+const logTime = (name) => {
   console.log(`${name}'s time to train is: ${days} days`);
 };
 
