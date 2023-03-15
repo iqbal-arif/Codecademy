9f51eef7ecd7a3e9d0fbbd0eda08cae664eb76ee
Arif Iqbal <arif_iqbal@yahoo.com>
2023-03-14 20:11:49 -0600

    new file:   JavaScript/Projects/Secret Message/secret.js

---
+++ JavaScript/Projects/Secret Message/secret.js
@@ -0,0 +1,32 @@
+let secretMessage = [
+  'Learning',
+  'is',
+  'not',
+  'about',
+  'what',
+  'you',
+  'get',
+  'easily',
+  'the',
+  'first',
+  'time,',
+  'it',
+  'is',
+  'about',
+  'what',
+  'you',
+  'can',
+  'figure',
+  'out.',
+  '-2015,',
+  'Chris',
+  'Pine,',
+  'Learn',
+  'JavaScript',
+];
+
+const removedMessage = secretMessage.pop();
+console.log(secretMessage.length);
+
+secretMessage.push('to', 'Program');
+console.log(secretMessage);
