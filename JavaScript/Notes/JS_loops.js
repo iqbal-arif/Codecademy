f99f6df933625c2a226d0f8eec2eb526fab935ad
Arif Iqbal <arif_iqbal@yahoo.com>
2023-03-15 21:58:07 -0600

    modified:   JavaScript/Notes/JS_loops.js

--- JavaScript/Notes/JS_loops.js
+++ JavaScript/Notes/JS_loops.js
@@ -61,3 +61,14 @@ const vacationSpots = ['Bali', 'Paris', 'Tulum'];
 for (let i = 0; i < vacationSpots.length; i++) {
   console.log(`I would love to visit ${vacationSpots[i]}`);
 }
+
+// Nested Loop Example
+const myArray = [6, 19, 20];
+const yourArray = [19, 81, 2];
+for (let i = 0; i < myArray.length; i++) {
+  for (let j = 0; j < yourArray.length; j++) {
+    if (myArray[i] === yourArray[j]) {
+      console.log('Both arrays have the number: ' + yourArray[j]);
+    }
+  }
+}
