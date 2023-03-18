1a0b20de0fcd388bdb22be43c18774075ace4247
Arif Iqbal <arif_iqbal@yahoo.com>
2023-03-17 21:41:03 -0600

    modified:   Back_End_Engineer/Challenges/howold.js

--- Back_End_Engineer/Challenges/howold.js
+++ Back_End_Engineer/Challenges/howold.js
@@ -1,4 +1,22 @@
 // Write your function here:
+/***********************************************/
+/***************HOW OLD ARE YOU*****************/
+/***********************************************/
+/*
+Write a function, howOld(), that has two number parameters, age and year, and returns how old someone who is currently that age was (or will be) during that year. Handle three different cases:
+
+    1. If the year is in the future, you should return a string in the following format:
+
+    'You will be [calculated age] in the year [year passed in]'
+
+    2. If the year is before they were born, you should return a string in the following format:
+
+    'The year [year passed in] was [calculated number of years] years before you were born'
+
+    3. If the year is in the past but not before the person was born, you should return a string in the following format:
+
+    'You were [calculated age] in the year [year passed in]'
+*/
 
 //Current Year & Month
 const currentDate = new Date(2023, 02);
