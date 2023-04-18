6aa02bdc59c6368dfbdcd0f7aef6ce0f7dcb4943
Arif Iqbal <arif_iqbal@yahoo.com>
2023-04-17 20:46:56 -0600

    modified:   JavaScript/Notes/JS_loops.js

---
+++ JavaScript/Projects/Whale Talk/whale.talk.js
@@ -0,0 +1,17 @@
+/*
+*********************Whale Talk*********************
+
+Take a phrase like ‘turpentine and turtles’ and translate it into its “whale talk” equivalent: ‘UUEEIEEAUUEE’.
+
+There are a few simple rules for translating text to whale language:
+
+    1. There are no consonants. Only vowels excluding “y”.
+    2. The u‘s and e‘s are extra long, so we must double them in our program.
+
+Once we have converted text to the whale language, the result is sung slowly, as is a custom in the ocean.
+
+To accomplish this translation, we can use our knowledge of loops. Let’s get started!
+*/
+
+let input = 'Vowel Conversion';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
