eb4230c15d2af318523c6578a7b01c6d05a0c146
Arif Iqbal <arif_iqbal@yahoo.com>
2023-04-17 22:43:39 -0600

    modified:   JavaScript/Quizes/js_loops.qz.js

--- JavaScript/Quizes/js_loops.qz.js
+++ JavaScript/Quizes/js_loops.qz.js
@@ -78,3 +78,18 @@ for (
 /*
 'Instagram', 'Facebook', 'Twitter'
 */
+
+/*
+Q8: What would the output of this block of code be?
+*/
+const animal = 'cat';
+
+for (let i = 0; i < animal.length; i++) {
+  console.log(animal[i]);
+  for (let j = 1; j < 4; j++) {
+    console.log(j);
+  }
+}
+/*
+c 1 2 3 a 1 2 3 t 1 2 3
+*/
