8812edbedcd9fe43114ff733ac74f3fadfaba302
Arif Iqbal <arif_iqbal@yahoo.com>
2023-04-17 20:47:05 -0600

    modified:   JavaScript/Notes/JS_loops.js

--- JavaScript/Notes/JS_loops.js
+++ JavaScript/Notes/JS_loops.js
@@ -195,9 +195,26 @@ const rapperArray = ["Lil' Kim", 'Jay-Z', 'Notorious B.I.G.', 'Tupac'];
 // Write your code below
 
 for (let i = 0; i < rapperArray.length; i++) {
-  // if (i > rapperArray[i] ) {
-  //  break;
-  // }
-  console.log(rapperArray[i]);
+  if (rapperArray[i] === 'Notorious B.I.G.') {
+    console.log(rapperArray[i]);
+
+    break;
+  }
 }
 console.log("And if you don't know, now you know.");
+
+/*
+Review
+
+Great job! In this lesson, we learned how to write cleaner code with loops. You now know:
+
+    Loops perform repetitive actions so we don’t have to code that process manually every time.
+    How to write for loops with an iterator variable that increments or decrements
+    How to use a for loop to iterate through an array
+    A nested for loop is a loop inside another loop
+    while loops allow for different types of stopping conditions
+    Stopping conditions are crucial for avoiding infinite loops.
+    do...while loops run code at least once— only checking the stopping condition after the first execution
+    The break keyword allows programs to leave a loop during the execution of its block
+
+*/
