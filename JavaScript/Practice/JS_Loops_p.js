1bed930c24c34f0673145a6a890f9befc489ea2b
Arif Iqbal <arif_iqbal@yahoo.com>
2023-04-17 21:42:53 -0600

    modified:   JavaScript/Practice/JS_Loops_p.js

--- JavaScript/Practice/JS_Loops_p.js
+++ JavaScript/Practice/JS_Loops_p.js
@@ -1,10 +1,29 @@
 /*********************Nested For Loop*****************/
 
-
 for (let outer = 0; outer < 2; outer += 1) {
   for (let inner = 0; inner < 3; inner += 1) {
     console.log(`${outer}-${inner}`);
   }
 }
+/*
+A nested for loop is when a for loop runs inside another for loop.
 
-Nested For Loops
+The inner loop will run all its iterations for each iteration of the outer loop.
+*/
+
+/*********************Break Keyword*****************/
+for (let i = 0; i < 99; i += 1) {
+  if (i > 5) {
+    break;
+  }
+  console.log(i);
+}
+
+/*
+Within a loop, the break keyword may be used to exit the loop immediately, continuing execution after the loop body.
+
+Here, the break keyword is used to exit the loop when i is greater than 5.
+
+*/
+
+/*********************Break Keyword*****************/
