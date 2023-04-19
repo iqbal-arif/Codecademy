1a81514f87bdd67ceab865f3aac21b675aa136fd
Arif Iqbal <arif_iqbal@yahoo.com>
2023-04-18 22:57:04 -0600

    modified:   JavaScript/Notes/JS_higherOrderFunctions.js

--- JavaScript/Notes/JS_higherOrderFunctions.js
+++ JavaScript/Notes/JS_higherOrderFunctions.js
@@ -99,3 +99,22 @@ higherOrderFunc(() => {
 /*
 In this example, we invoked higherOrderFunc() with an anonymous function (a function without a name) that counts to 10. Anonymous functions can be arguments too!
 */
+
+/*
+Review
+******
+******
+Great job! By thinking about functions as data, and learning about higher-order functions, you’ve taken important steps in learning to write clean, modular code that takes advantage of JavaScript’s flexibility.
+
+Let’s review what we learned in this lesson:
+
+    1. Abstraction allows us to write complicated code in a way that’s easy to reuse, debug, and understand for human readers.
+
+    2. We can work with functions the same way we work with any other type of data, including reassigning them to new variables.
+
+    3. JavaScript functions are first-class objects, so they have properties and methods like any other object.
+
+    4. Functions can be passed into other functions as parameters.
+
+    5. A higher-order function is a function that either accepts functions as parameters, returns a function, or both.
+*/
