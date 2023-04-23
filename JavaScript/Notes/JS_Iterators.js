a6cfb9e4ec938578ee7bc0521ccfa34b7b5fde81
Arif Iqbal <arif_iqbal@yahoo.com>
2023-04-22 21:24:39 -0600

    modified:   JavaScript/Notes/JS_Iterators.js

--- JavaScript/Notes/JS_Iterators.js
+++ JavaScript/Notes/JS_Iterators.js
@@ -504,3 +504,24 @@ const smallerNums = nums.map((num) => num - 5);
 
 // Choose a method that will return a boolean value
 nums.every((num) => num < 0);
+
+/*
+Review
+******
+******
+Awesome job on clearing the iterators lesson! You have learned a number of useful methods in this lesson as well as how to use the JavaScript documentation from the Mozilla Developer Network to discover and understand additional methods. Let’s review!
+
+    1. .forEach() is used to execute the same code on every element in an array but does not change the array and returns undefined.
+
+    2. .map() executes the same code on every element in an array and returns a new array with the updated elements.
+
+    3. .filter() checks every element in an array to see if it meets certain criteria and returns a new array with the elements that return truthy for the criteria.
+
+    4. .findIndex() returns the index of the first element of an array that satisfies a condition in the callback function. It returns -1 if none of the elements in the array satisfies the condition.
+
+    5. .reduce() iterates through an array and takes the values of the elements and returns a single value.
+
+    6. All iterator methods take a callback function, which can be a pre-defined function, a function expression, or an arrow function.
+
+    7. You can visit the Mozilla Developer Network to learn more about iterator methods (and all other parts of JavaScript!).
+*/
