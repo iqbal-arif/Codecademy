188f2509e8bcf9d74c35d9e8ff5c3665bd0b2b0c
Arif Iqbal <arif_iqbal@yahoo.com>
2023-04-26 06:31:23 -0600

    modified:   JavaScript/Notes/JS_objects.js

--- JavaScript/Notes/JS_objects.js
+++ JavaScript/Notes/JS_objects.js
@@ -95,3 +95,52 @@ returnAnyProp(spaceship, 'homePlanet'); // Returns 'Earth'
 /*
 If we tried to write our returnAnyProp() function with dot notation (objectName.propName) the computer would look for a key of 'propName' on our object and not the value of the propName parameter. v
 */
+
+/*
+5. Property Assignment
+**********************
+**********************
+
+Once we’ve defined an object, we’re not stuck with all the properties we wrote. Objects are mutable meaning we can update them after we create them!
+
+We can use either dot notation, ., or bracket notation, [], and the assignment operator, = to add new key-value pairs to an object or change an existing property.
+
+diagram showing how an object followed by brackets ([]) with the property name as a string can be reassigned to a new value. This same idea applies for accessing a property using dot notation which has the object name, followed by a dot and the name of the property
+
+One of two things can happen with property assignment:
+
+    1. If the property already exists on the object, whatever value it held before will be replaced with the newly assigned value.
+    2. If there was no property with that name, a new property will be added to the object.
+
+It’s important to know that although we can’t reassign an object declared with const, we can still mutate it, meaning we can add new properties and change the properties that are there.
+*/
+const spaceship3 = { type: 'shuttle' };
+spaceship3 = { type: 'alien' }; // TypeError: Assignment to constant variable.
+spaceship3.type = 'alien'; // Changes the value of the type property
+spaceship3.speed = 'Mach 5'; // Creates a new key of 'speed' with a value of 'Mach 5'
+/*
+You can delete a property from an object with the delete operator.
+*/
+const spaceship4 = {
+  'Fuel Type': 'Turbo Fuel',
+  homePlanet: 'Earth',
+  mission: 'Explore the universe',
+};
+
+delete spaceship4.mission; // Removes the mission property
+
+/**********Practice*********/
+
+let spaceship5 = {
+  'Fuel Type': 'Turbo Fuel',
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Secret Mission': 'Discover life outside of Earth.',
+};
+
+// Write your code below
+spaceship5.color = 'glorious gold';
+
+spaceship5.numEngines = 7;
+
+delete spaceship5['Secret Mission'];
