790bb9fdccc695bf4e953e09957994fe06343522
Arif Iqbal <arif_iqbal@yahoo.com>
2023-04-26 07:03:25 -0600

    modified:   JavaScript/Notes/JS_objects.js

--- JavaScript/Notes/JS_objects.js
+++ JavaScript/Notes/JS_objects.js
@@ -144,3 +144,82 @@ spaceship5.color = 'glorious gold';
 spaceship5.numEngines = 7;
 
 delete spaceship5['Secret Mission'];
+
+/*
+6. Methods
+**********
+**********
+
+When the data stored on an object is a function we call that a method. A property is what an object has, while a method is what an object does.
+
+Do object methods seem familiar? That’s because you’ve been using them all along! For example console is a global JavaScript object and .log() is a method on that object. Math is also a global JavaScript object and .floor() is a method on it.
+
+We can include methods in our object literals by creating ordinary, colon-separated key-value pairs. The key serves as our method’s name, while the value is an anonymous function expression.
+*/
+const alienShip = {
+  invade: function () {
+    console.log(
+      'Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.'
+    );
+  },
+};
+/*
+With the new method syntax introduced in ES6 we can omit the colon and the function keyword.
+*/
+const alienShip = {
+  invade() {
+    console.log(
+      'Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.'
+    );
+  },
+};
+/*
+Object methods are invoked by appending the object’s name with the dot operator followed by the method name and parentheses:
+*/
+alienShip.invade(); // Prints 'Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.'
+
+/*
+Nested Objects
+
+In application code, objects are often nested— an object might have another object as a property which in turn could have a property that’s an array of even more objects!
+
+In our spaceship object, we want a crew object. This will contain all the crew members who do important work on the craft. Each of those crew members are objects themselves. They have properties like name, and degree, and they each have unique methods based on their roles. We can also nest other objects in the spaceship such as a telescope or nest details about the spaceship’s computers inside a parent nanoelectronics object.
+
+const spaceship = {
+     telescope: {
+        yearBuilt: 2018,
+        model: '91031-XLT',
+        focalLength: 2032 
+     },
+    crew: {
+        captain: { 
+            name: 'Sandra', 
+            degree: 'Computer Engineering', 
+            encourageTeam() { console.log('We got this!') } 
+         }
+    },
+    engine: {
+        model: 'Nimbus2000'
+     },
+     nanoelectronics: {
+         computer: {
+            terabytes: 100,
+            monitors: 'HD'
+         },
+        'back-up': {
+           battery: 'Lithium',
+           terabytes: 50
+         }
+    }
+}; 
+
+We can chain operators to access nested properties. We’ll have to pay attention to which operator makes sense to use in each layer. It can be helpful to pretend you are the computer and evaluate each expression from left to right so that each operation starts to feel a little more manageable.
+
+spaceship.nanoelectronics['back-up'].battery; // Returns 'Lithium'
+
+In the preceding code:
+
+    First the computer evaluates spaceship.nanoelectronics, which results in an object containing the back-up and computer objects.
+    We accessed the back-up object by appending ['back-up'].
+    The back-up object has a battery property, accessed with .battery which returned the value stored there: 'Lithium'
+
