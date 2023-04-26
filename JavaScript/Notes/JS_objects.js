6e17608624715166dfeccb71e2b10d697b6f6ec7
Arif Iqbal <arif_iqbal@yahoo.com>
2023-04-26 13:40:51 -0600

    modified:   JavaScript/Notes/JS_objects.js

--- JavaScript/Notes/JS_objects.js
+++ JavaScript/Notes/JS_objects.js
@@ -129,8 +129,9 @@ const spaceship4 = {
 
 delete spaceship4.mission; // Removes the mission property
 
+/***************************/
 /**********Practice*********/
-
+/**************************/
 let spaceship5 = {
   'Fuel Type': 'Turbo Fuel',
   homePlanet: 'Earth',
@@ -226,3 +227,51 @@ In the preceding code:
     2. We accessed the back-up object by appending ['back-up'].
     3. The back-up object has a battery property, accessed with .battery which returned the value stored there: 'Lithium'
 */
+/***************************/
+/**********Practice*********/
+/**************************/
+
+let spaceship7 = {
+  passengers: null,
+  telescope: {
+    yearBuilt: 2018,
+    model: '91031-XLT',
+    focalLength: 2032,
+  },
+  crew: {
+    captain: {
+      name: 'Sandra',
+      degree: 'Computer Engineering',
+      encourageTeam() {
+        console.log('We got this!');
+      },
+      'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'],
+    },
+  },
+  engine: {
+    model: 'Nimbus2000',
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: 'HD',
+    },
+    'back-up': {
+      battery: 'Lithium',
+      terabytes: 50,
+    },
+  },
+};
+
+/*
+  Create a variable capFave and assign the captain‘s favorite food (the element in the 0th index of her 'favorite foods' array) to it. Make sure to use bracket and dot notation to get the value of the food through nested access (don’t just copy the value into the variable!)
+  */
+const capFave = spaceship7.crew.captain['favorite foods'][0];
+/*
+  Right now the passengers property has a value of null. Instead, assign as its value an array of objects. These objects should represent the spaceship‘s passengers as individual objects. Make at least one passenger object in the array that has at least one key-value pair on it. 
+  */
+spaceship7.passengers = [{ hobbies: 'football' }];
+/*
+  Create a variable firstPassenger and assign the first passenger as its value (the element in the 0th index of the spaceship.passengers array you just made). Make sure to use bracket and dot notation to get the passenger object through nested access (don’t just copy the object into the variable!)
+  */
+const firstPassenger = spaceship7.passengers[0];
