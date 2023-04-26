ca431c6e38e3f7051fc3a9af47ee49c4378a50c3
Arif Iqbal <arif_iqbal@yahoo.com>
2023-04-26 16:59:13 -0600

    modified:   JavaScript/Notes/JS_objects.js

--- JavaScript/Notes/JS_objects.js
+++ JavaScript/Notes/JS_objects.js
@@ -275,3 +275,54 @@ spaceship7.passengers = [{ hobbies: 'football' }];
   Create a variable firstPassenger and assign the first passenger as its value (the element in the 0th index of the spaceship.passengers array you just made). Make sure to use bracket and dot notation to get the passenger object through nested access (don’t just copy the object into the variable!)
   */
 const firstPassenger = spaceship7.passengers[0];
+
+/*
+8. Pass By Reference
+********************
+********************
+Objects are passed by reference. This means when we pass a variable assigned to an object into a function as an argument, the computer interprets the parameter name as pointing to the space in memory holding that object. As a result, functions which change object properties actually mutate the object permanently (even when the object is assigned to a const variable).
+*/
+const spaceship8 = {
+  homePlanet: 'Earth',
+  color: 'silver',
+};
+
+let paintIt = (obj) => {
+  obj.color = 'glorious gold';
+};
+
+paintIt(spaceship8);
+
+spaceship8.color; // Returns 'glorious gold'
+
+/*
+Our function paintIt() permanently changed the color of our spaceship object. However, reassignment of the spaceship variable wouldn’t work in the same way:
+*/
+let spaceship9 = {
+  homePlanet: 'Earth',
+  color: 'red',
+};
+let tryReassignment = (obj) => {
+  obj = {
+    identified: false,
+    'transport type': 'flying',
+  };
+  console.log(obj); // Prints {'identified': false, 'transport type': 'flying'}
+};
+tryReassignment(spaceship9); // The attempt at reassignment does not work.
+spaceship9; // Still returns {homePlanet : 'Earth', color : 'red'};
+
+spaceship9 = {
+  identified: false,
+  'transport type': 'flying',
+}; // Regular reassignment still works.
+/*
+Let’s look at what happened in the code example:
+
+    1. We declared this spaceship object with let. This allowed us to reassign it to a new object with identified and 'transport type' properties with no problems.
+    2. When we tried the same thing using a function designed to reassign the object passed into it, the reassignment didn’t stick (even though calling console.log() on the object produced the expected result).
+    3. When we passed spaceship into that function, obj became a reference to the memory location of the spaceship object, but not to the spaceship variable. This is because the obj parameter of the tryReassignment() function is a variable in its own right. The body of tryReassignment() has no knowledge of the spaceship variable at all!
+    4. When we did the reassignment in the body of tryReassignment(), the obj variable came to refer to the memory location of the object {'identified' : false, 'transport type' : 'flying'}, while the spaceship variable was completely unchanged from its earlier value.
+
+
+*/
