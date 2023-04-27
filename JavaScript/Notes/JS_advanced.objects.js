94260af4f0f99e10c4240421205123451eda9262
Arif Iqbal <arif_iqbal@yahoo.com>
2023-04-27 14:38:50 -0600

    modified:   JavaScript/Notes/JS_advanced.objects.js

--- JavaScript/Notes/JS_advanced.objects.js
+++ JavaScript/Notes/JS_advanced.objects.js
@@ -188,3 +188,40 @@ Now that we’ve gone over syntax, let’s discuss some notable advantages of us
 
 Another thing to keep in mind when using getter (and setter) methods is that properties cannot share the same name as the getter/setter function. If we do so, then calling the method will result in an infinite call stack error. One workaround is to add an underscore before the property name like we did in the example above. 
 */
+
+/*
+***********
+5. Setters
+***********
+
+Along with getter methods, we can also create setter methods which reassign values of existing properties within an object. Let’s see an example of a setter method:
+*/
+const person2 = {
+  _age: 37,
+  set age(newAge) {
+    if (typeof newAge === 'number') {
+      this._age = newAge;
+    } else {
+      console.log('You must assign a number to age');
+    }
+  },
+};
+/*
+Notice that in the example above:
+
+    1. We can perform a check for what value is being assigned to this._age.
+    2. When we use the setter method, only values that are numbers will reassign this._age
+    3. There are different outputs depending on what values are used to reassign this._age.
+
+Then to use the setter method:
+*/
+person.age = 40;
+console.log(person._age); // Logs: 40
+person.age = '40'; // Logs: You must assign a number to age
+/*
+Setter methods like age do not need to be called with a set of parentheses. Syntactically, it looks like we’re reassigning the value of a property.
+
+Like getter methods, there are similar advantages to using setter methods that include checking input, performing actions on properties, and displaying a clear intention for how the object is supposed to be used. Nonetheless, even with a setter method, it is still possible to directly reassign properties. For example, in the example above, we can still set ._age directly:
+*/
+person._age = 'forty-five';
+console.log(person._age); // Prints forty-five
