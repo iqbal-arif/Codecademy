6a2d80fc5939980f02cb985333b051185a191c6b
Arif Iqbal <arif_iqbal@yahoo.com>
2023-04-28 16:06:15 -0600

    modified:   JavaScript/Notes/JS_advanced.objects.js

--- JavaScript/Notes/JS_advanced.objects.js
+++ JavaScript/Notes/JS_advanced.objects.js
@@ -339,3 +339,63 @@ const robotFactory = (model, mobile) => {
 const tinCan = robotFactory('P-500', true);
 
 tinCan.beep();
+
+/*
+***************************
+7. Property Value Shorthand
+***************************
+ES6 introduced some new shortcuts for assigning properties to variables known as destructuring.
+
+In the previous exercise, we created a factory function that helped us create objects. We had to assign each property a key and value even though the key name was the same as the parameter name we assigned to it. To remind ourselves, here’s a truncated version of the factory function:
+*/
+const monsterFactory2 = (name, age) => {
+  return {
+    name: name,
+    age: age,
+  };
+};
+/*
+Imagine if we had to include more properties, that process would quickly become tedious! But we can use a destructuring technique, called property value shorthand, to save ourselves some keystrokes. The example below works exactly like the example above:
+*/
+const monsterFactory3 = (name, age) => {
+  return {
+    name,
+    age,
+  };
+};
+/*
+Notice that we don’t have to repeat ourselves for property assignments!
+*/
+
+/*
+**************************
+8. Destructured Assignment
+**************************
+
+We often want to extract key-value pairs from objects and save them as variables. Take for example the following object:
+*/
+const vampire = {
+  name: 'Dracula',
+  residence: 'Transylvania',
+  preferences: {
+    day: 'stay inside',
+    night: 'satisfy appetite',
+  },
+};
+/*
+If we wanted to extract the residence property as a variable, we could use the following code:
+*/
+const residence = vampire.residence;
+console.log(residence); // Prints 'Transylvania'
+/*
+However, we can also take advantage of a destructuring technique called destructured assignment to save ourselves some keystrokes. In destructured assignment we create a variable with the name of an object’s key that is wrapped in curly braces { } and assign to it the object. Take a look at the example below:
+*/
+const { residence2 } = vampire;
+console.log(residence2); // Prints 'Transylvania'
+/*
+Look back at the vampire object’s properties in the first code example. Then, in the example above, we declare a new variable residence that extracts the value of the residence property of vampire. When we log the value of residence to the console, 'Transylvania' is printed.
+
+We can even use destructured assignment to grab nested properties of an object:
+*/
+const { day } = vampire.preferences;
+console.log(day); // Prints 'stay inside'
