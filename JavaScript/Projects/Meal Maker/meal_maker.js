0fa7f1fc7c2b38b48c5dd808572f05245d1e36ed
Arif Iqbal <arif_iqbal@yahoo.com>
2023-04-29 21:41:32 -0600

    modified:   JavaScript/Projects/Meal Maker/meal_maker.js

--- JavaScript/Projects/Meal Maker/meal_maker.js
+++ JavaScript/Projects/Meal Maker/meal_maker.js
@@ -7,3 +7,38 @@ A restaurant has hired you to create a function for their website that allows th
 If you get stuck during this project or would like to see an experienced developer work through it, click “Get Unstuck” to see a project walkthrough video.
 
 */
+
+/*
+
+1.
+
+We’ll hold the meal, price, and their respective getters and setters in an object named menu.
+
+In app.js, create an empty menu object.
+2.
+
+The menu object will hold the meal and price of Today’s Special as properties and they shouldn’t be altered directly.
+
+Within the menu object, create a _meal property with a value of an empty string (''). This will eventually hold the name of the meal.
+3.
+
+Next, add a _price property with a value of 0. This will eventually hold the price of the meal, and should also not be altered directly.
+4.
+
+We know properties that begin with _ should not be directly manipulated. But just to validate this knowledge, let’s test it out!
+
+Below the menu object, directly manipulate the two properties by assigning _meal a number value and _price a string value.
+
+Then, below the new assignments, console.log() the menu object to see how not type checking these values could cause confusion for a website visitor!
+
+*/
+
+const menu = {
+  _meal: ' ',
+  _price: 0,
+};
+
+menu._meal = 10;
+menu._price = 'price';
+
+console.log(menu);
