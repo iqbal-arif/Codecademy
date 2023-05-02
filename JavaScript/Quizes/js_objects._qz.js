d5d95640bc5ada18dec4161a80e90a1c0681e427
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-01 21:14:21 -0600

    modified:   JavaScript/Quizes/js_objects._qz.js

--- JavaScript/Quizes/js_objects._qz.js
+++ JavaScript/Quizes/js_objects._qz.js
@@ -2,3 +2,37 @@
 Q1: What is a factory function?
 A1: A function that returns an object.
 */
+
+/*
+Q2: How can we add a property to the object below?
+*/
+let bikes = {
+  schwinn: 'blue',
+  trek: 'black',
+};
+
+// A2:
+bikes['specialized'] = 'red';
+
+/*
+Q3: Which line of code would NOT print the value saved to the _num key in the tempObj object.
+*/
+let tempObj = {
+  _num: 22,
+  get num() {
+    return this._num;
+  },
+};
+// A3:
+console.log(tempObj.num());
+
+/*
+Q4: What should we add to the empty .withDiscount() method to return the cost of the meatballs object with a two dollar discount?
+*/
+let meatballs = {
+  cost: 5,
+  withDiscount() {},
+};
+
+// A4:
+return this.cost - 2;
