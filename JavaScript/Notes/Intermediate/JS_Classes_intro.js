259267ccbe4446631ae5ca42735728ef0b3c7c6b
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-03 21:30:21 -0600

    modified:   JavaScript/Notes/Intermediate/JS_Classes_intro.js

--- JavaScript/Notes/Intermediate/JS_Classes_intro.js
+++ JavaScript/Notes/Intermediate/JS_Classes_intro.js
@@ -783,3 +783,51 @@ This Dog class has access to the same properties, getters, setters, and methods 
 
 Now that we’ve abstracted animal daycare features, it’s easy to see how you can extend Animal to support other classes, like Rabbit, Bird or even Snake.
 */
+
+/************************************/
+/**************Practice**************/
+/************************************/
+
+/*
+1. Use the properties and methods below to help you complete the tasks that follow.
+Nurse
+
+    Properties: _name, _remainingVacationDays (set to 20 inside the constructor()), _certifications
+    Methods: .takeVacationDays(), .addCertification()
+
+Under the Nurse constructor(), add a getter that returns the value saved to the Nurse instance’s _certifications.
+
+*/
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  }
+  get certifications() {
+    return (this._certifications = certifications);
+  }
+}
+
+const nurseOlynyk2 = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
