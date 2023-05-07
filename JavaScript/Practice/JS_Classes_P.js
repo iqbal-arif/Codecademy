76a87c675832a0ed33adad0ad58b933fde7a44d5
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-06 21:42:11 -0600

    modified:   JavaScript/Practice/JS_Classes_P.js

--- JavaScript/Practice/JS_Classes_P.js
+++ JavaScript/Practice/JS_Classes_P.js
@@ -27,3 +27,29 @@ Dog.bark();
 Within a JavaScript class, the static keyword defines a static method for a class. Static methods are not called on individual instances of the class, but are called on the class itself. Therefore, they tend to be general (utility) methods.
 
 */
+
+/*
+*********
+2. Class
+*********
+*/
+class Song {
+  constructor() {
+    this.title;
+    this.author;
+  }
+
+  play() {
+    console.log('Song playing!');
+  }
+}
+
+const mySong = new Song();
+mySong.play();
+/*
+JavaScript supports the concept of classes as a syntax for creating objects. Classes specify the shared properties and methods that objects produced from the class will have.
+
+When an object is created based on the class, the new object is referred to as an instance of the class. New instances are created using the new keyword.
+
+The code sample shows a class that represents a Song. A new object called mySong is created underneath and the .play() method on the class is called. The result would be the text Song playing! printed in the console.
+*/
