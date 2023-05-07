94ede599b6d197d4ad55b4f08a8ccfc01ac1149d
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-06 21:44:12 -0600

    modified:   JavaScript/Practice/JS_Classes_P.js

--- JavaScript/Practice/JS_Classes_P.js
+++ JavaScript/Practice/JS_Classes_P.js
@@ -71,3 +71,35 @@ class Song {
 /*
 Properties in objects are separated using commas. This is not the case when using the class syntax. Methods in classes do not have any separators between them.
 */
+
+/*
+***********
+4. extends
+***********
+*/
+// Parent class
+class Media {
+  constructor(info) {
+    this.publishDate = info.publishDate;
+    this.name = info.name;
+  }
+}
+
+// Child class
+class Song extends Media {
+  constructor(songData) {
+    super(songData);
+    this.artist = songData.artist;
+  }
+}
+
+const mySong = new Song({
+  artist: 'Queen',
+  name: 'Bohemian Rhapsody',
+  publishDate: 1975,
+});
+/*
+JavaScript classes support the concept of inheritance — a child class can extend a parent class. This is accomplished by using the extends keyword as part of the class definition.
+
+Child classes have access to all of the instance properties and methods of the parent class. They can add their own properties and methods in addition to those. A child class constructor calls the parent class constructor using the super() method
+*/
