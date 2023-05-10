f39241dbc59f72f82de4652741909f65bb3f75d2
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-10 14:53:59 -0600

    new file:   JavaScript/Notes/Intermediate/module.export.ways.js

---
+++ JavaScript/Notes/Intermediate/module.export.ways.js
@@ -0,0 +1,37 @@
+<html>
+  <head>
+    <title>Secret Image</title>
+  </head>
+  <body>
+    <button id="secret-button"> Want to see something cool? </button>
+    <img id="secret-img" src="imageURL.jpg" style="display: none">
+    <script src="./secret-image.js"> </script>
+  </body>
+</html>
+
+/* secret-image.js */
+const buttonElement = document.getElementById('secret-button');
+const imgElement = document.getElementById('secret-img');
+ 
+const toggleHiddenElement2 = (domElement) => {
+    if (domElement.style.display === 'none') {
+      domElement.style.display = 'block';
+    } else {
+      domElement.style.display = 'none';
+    }
+}
+ 
+buttonElement.addEventListener('click', () => {
+  toggleHiddenElement2(imgElement);
+});
+
+/*
+secret-image/
+|-- secret-image.html
+|-- secret-image.js
+secret-messages/
+|-- secret-messages.html
+|-- secret-messages.js
+modules/
+|-- dom-functions.js    <-- new module file
+*/
