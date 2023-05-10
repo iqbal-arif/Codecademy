1ca739672d3450f7fdbabc142daf71a85409b8b8
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-09 19:28:55 -0600

    modified:   JavaScript/Notes/Intermediate/JS_ESC_Modules_implement.js

--- JavaScript/Notes/Intermediate/JS_ESC_Modules_implement.js
+++ JavaScript/Notes/Intermediate/JS_ESC_Modules_implement.js
@@ -295,13 +295,12 @@ Note: Upon completing this challenge, the text will change to “I did it!” an
 /* main.js */
 
 // import the functions here, then uncomment the code below
-
+import { changeText, changeToFunkyColor } from './module.js';
 const header = document.getElementById("header");
 
 // call changeText here
-
+changeText(header,"I did it!")
 setInterval(()=> {
-  
   // call changeToFunkyColor() here
-
+changeToFunkyColor(header)
 }, 200);
