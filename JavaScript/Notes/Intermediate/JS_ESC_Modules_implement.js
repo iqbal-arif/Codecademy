e62f890f3aecb327b47b0e50b00e8a2698375fd8
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-09 19:33:22 -0600

    modified:   JavaScript/Notes/Intermediate/JS_ESC_Modules_implement.js

--- JavaScript/Notes/Intermediate/JS_ESC_Modules_implement.js
+++ JavaScript/Notes/Intermediate/JS_ESC_Modules_implement.js
@@ -304,3 +304,12 @@ setInterval(()=> {
   // call changeToFunkyColor() here
 changeToFunkyColor(header)
 }, 200);
+
+/*
+***************************
+I. ES6 Modules Challenge #3
+***************************
+The <script> element below loads a JavaScript file which makes use of ES6 import/export syntax. Which of the following changes must be made in order to properly load this module and avoid browser errors?
+A type = "module" attribute must be added to the opening <script> tag.
+*/
+<script type="module" src="./my-module.js"> </script>
