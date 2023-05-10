35a23c5edc8924656a89f9c9a150f98f6abd4b0e
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-09 19:48:24 -0600

    modified:   JavaScript/Notes/Intermediate/JS_ESC_Modules_implement.js

--- JavaScript/Notes/Intermediate/JS_ESC_Modules_implement.js
+++ JavaScript/Notes/Intermediate/JS_ESC_Modules_implement.js
@@ -277,7 +277,7 @@ function changeToFunkyColor(domElement) {
 export {changeText, changeToFunkyColor}
 /*
 ***************************
-I. ES6 Modules Challenge #2
+II. ES6 Modules Challenge #2
 ***************************
 You are building a simple webpage that makes use of the module module.js. This module declares and exports two functions whose signatures are listed below:
 
@@ -307,9 +307,168 @@ changeToFunkyColor(header)
 
 /*
 ***************************
-I. ES6 Modules Challenge #3
+III. ES6 Modules Challenge #3
 ***************************
 The <script> element below loads a JavaScript file which makes use of ES6 import/export syntax. Which of the following changes must be made in order to properly load this module and avoid browser errors?
 A type = "module" attribute must be added to the opening <script> tag.
+Correct! Forgetting to add this attribute may cause your browser to throw an error.
 */
 <script type="module" src="./my-module.js"> </script>
+/*
+**********************************************
+7. Renaming Imports to Avoid Naming Collisions
+**********************************************
+Inevitably, you will run into a situation where the resources you wish to import share a name with some other value that already exists in your program (or from another imported module).
+
+For example, suppose you had access to two modules, greeterEspanol.js and greeterFrancais.js. Each exports a function called greet():
+*/
+/* inside greeterEspanol.js */
+const greet1 = () => {
+  console.log('hola');
+}
+export { greet };
+ 
+/* inside greeterFrancais.js */
+const greet2 = () => {
+  console.log('bonjour');
+}
+export { greet };
+/*
+Now, let’s say you wanted to use each of these functions in a program, called main.js, that simulates a conversation between a spanish-speaker and a french-speaker.
+*/
+import { greet } from 'greeterEspanol.js';
+import { greet } from 'greeterFrancais.js';
+/*
+The code above will throw an error on line 2 due to the fact that the identifier greet has already been defined on line 1. Thankfully, ES6 includes syntax for renaming imported resources by adding in the keyword as. It looks like this:
+*/
+import { exportedResource as newlyNamedResource } from '/path/to/module'
+/*
+Let’s see how this could work within main.js
+
+/* main.js */
+import { greet as greetEspanol } from 'greeterEspanol.js';
+import { greet as greetFrancais } from 'greeterFrancais.js';
+ 
+greetEspanol(); // Prints: hola
+greetFrancais(); // Prints: bonjour
+/*
+Now, both of the imported functions can be called within main.js using their new identifiers, greetEspanol() and greetFrancais().
+*/
+
+/*
+****************************
+IV. ES6 Modules Challenge #4
+****************************
+
+Consider the following functions being exported from the modules square-area.js and circle-area.js:
+*/
+
+/* From square-area.js... */
+export function area(side) {
+  return side * side;
+}
+ 
+/* From circle-area.js... */
+export function area(radius) {
+  return Math.PI * radius * radius;
+}
+/*
+The file below, area-calculator.js, is programmed to print the area of a square and a circle to the console using these functions. However, because the exported values share the same name, they need to be renamed when they are imported! Fill in the code blanks below with the correct syntax for renaming these functions.
+
+/* area-calculator.js */
+ 
+import { area as squareArea } from 'square-area.js';
+import { area as circleArea } from 'circle-area.js';
+ 
+console.log('The area of a square with sides of length 5 is ' + squareArea(5));
+console.log('The area of a circle with radius of length 5 is ' + circleArea(5));
+
+/*
+******************************
+8. Default Exports and Imports
+******************************
+So far, the examples shown have used the named export syntax, in which a module’s resources are exported individually by name. Every module also has the option to export a single value to represent the entire module called the default export. Often, though not always, the default export value is an object containing the entire set of functions and/or data values of a module.
+
+The syntax for exporting a default object looks like this:
+*/
+const resources2 = { 
+  valueA, 
+  valueB 
+}
+export { resources as default2 };
+/*
+With this syntax, the object containing the module’s resources is first declared and then is exported on the next line. At first glance, it looks like the resources object is being exported as a named export. However, the clause as default renames the exported object to default, a reserved identifier that can only be given to a single exported value.
+
+You may also see this shorthand syntax where the value follows export default is the default value to be exported:
+*/
+const resources = {
+  valueA,
+  valueB
+}
+export default resources;
+/*
+
+****************************
+9. Importing default values.
+****************************
+The syntax for importing default exports looks like this:
+*/
+import importedResources from 'module.js';
+/*
+Notice that the curly braces are gone from the import statement. This syntax is actually shorthand for import { default as importedResources } from 'module.js and the imported default value may be given any name the programmer chooses.
+
+It should be noted that if the default export is an object, the values inside cannot be extracted until after the object is imported, like so:
+
+// This will work...
+import resources from 'module.js'
+*/
+const { valueA, valueB } = resources;
+ 
+// This will not work...
+import { valueA, valueB } from 'module.js'
+/*
+Let’s return to the prior example. The dom-functions.js module from above could be rewritten to use default exports like so:
+*/
+/* dom-functions.js */
+const toggleHiddenElement4 = (domElement) => {
+    if (domElement.style.display === 'none') {
+      domElement.style.display = 'block';
+    } else {
+      domElement.style.display = 'none';
+    }
+}
+ 
+const changeToFunkyColor = (domElement) => {
+  const r = Math.random() * 255;
+  const g = Math.random() * 255;
+  const b = Math.random() * 255;
+ 
+  domElement.style.background = `rgb(${r}, ${g}, ${b})`;
+}
+ 
+const resources3 = { 
+  toggleHiddenElement, 
+  changeToFunkyColor
+}
+export default resources;
+/*
+This default exports object can now be used within secret-messages.js like so:
+*/
+import domFunctions from '../modules/dom-functions.js';
+ 
+const { toggleHiddenElement, changeToFunkyColor } = domFunctions;
+ 
+const buttonElement = document.getElementById('secret-button');
+const pElement = document.getElementById('secret-p');
+ 
+buttonElement.addEventListener('click', () => {
+  toggleHiddenElement(pElement);
+  changeToFunkyColor(buttonElement);
+});
+/*
+Notice how toggleHiddenElement() and changeToFunkyColor() are now methods of the imported object called domFunctions and are extracted using ES6 destructuring syntax! It should be noted that the identifier domFunctions can be chosen as the programmer sees fit. If you recall, the syntax used in the snippet above is shorthand for:
+*/
+import { default as domFunctions } from '../modules/dom-functions.js';
+/*
+Continue on to the challenges below before the final review at the end of the article.
+*/
