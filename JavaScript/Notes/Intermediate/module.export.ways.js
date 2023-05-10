7bfd2be926d8536a4c2f0c313e1a1ecf10d57169
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-10 14:59:01 -0600

    modified:   JavaScript/Notes/Intermediate/module.export.ways.js

--- JavaScript/Notes/Intermediate/module.export.ways.js
+++ JavaScript/Notes/Intermediate/module.export.ways.js
@@ -1,3 +1,5 @@
+/************************NAME EXPORT SYNTAX*******************************/
+
 <html>
   <head>
     <title>Secret Image</title>
@@ -35,3 +37,34 @@ secret-messages/
 modules/
 |-- dom-functions.js    <-- new module file
 */
+
+/* dom-functions.js */
+const toggleHiddenElement3 = (domElement) => {
+    if (domElement.style.display === 'none') {
+      domElement.style.display = 'block';
+    } else {
+      domElement.style.display = 'none';
+    }
+}
+ 
+const changeToFunkyColor3 = (domElement) => {
+  const r = Math.random() * 255;
+  const g = Math.random() * 255;
+  const b = Math.random() * 255;
+ 
+  domElement.style.background = `rgb(${r}, ${g}, ${b})`;
+}
+ 
+export { toggleHiddenElement3, changeToFunkyColor3 };
+/*
+Let’s briefly break down how this module works:
+
+    1. The function toggleHiddenElement() is declared. It accepts a domElement as an input and either shows or hides that element depending on its current display style value.
+    2. A new function changeToFunkyColor() is declared. It accepts a domElement as an input and then sets its background color to a random rgb() color value.
+    3. The two functions are exported using the ES6 export statement.
+
+These exported functions are now available to be imported and used by other files!
+
+/************************NAME EXPORT SYNTAX*******************************/
+/******export { toggleHiddenElement3, changeToFunkyColor3 };******/
+/************************NAME EXPORT SYNTAX*******************************/
