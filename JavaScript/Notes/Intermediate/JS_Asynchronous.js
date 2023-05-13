c76a8e570fea0249d57bc56f9371703022d735ac
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-12 20:32:33 -0600

    modified:   JavaScript/Notes/Intermediate/JS_Asynchronous.js

--- JavaScript/Notes/Intermediate/JS_Asynchronous.js
+++ JavaScript/Notes/Intermediate/JS_Asynchronous.js
@@ -132,3 +132,58 @@ const orderSunglasses = () => {
 const orderPromise = orderSunglasses();
 
 console.log(orderPromise);
+
+/*
+**********************************
+4. The Node setTimeout() Function
+**********************************
+Knowing how to construct a promise is useful, but most of the time, knowing how to consume, or use, promises will be key. Rather than constructing promises, you’ll be handling Promise objects returned to you as the result of an asynchronous operation. These promises will start off pending but settle eventually.
+
+Moving forward, we’ll be simulating this by providing you with functions that return promises which settle after some time. To accomplish this, we’ll be using setTimeout(). setTimeout() is a Node API (a comparable API is provided by web browsers) that uses callback functions to schedule tasks to be performed after a delay. setTimeout() has two parameters: a callback function and a delay in milliseconds.
+*/
+const delayedHello = () => {
+  console.log('Hi! This is an asynchronous greeting!');
+};
+
+setTimeout(delayedHello, 2000);
+/*
+Here, we invoke setTimeout() with the callback function delayedHello() and 2000. In at least two seconds delayedHello() will be invoked. But why is it “at least” two seconds and not exactly two seconds?
+
+This delay is performed asynchronously—the rest of our program won’t stop executing during the delay. Asynchronous JavaScript uses something called the event-loop. After two seconds, delayedHello() is added to a line of code waiting to be run. Before it can run, any synchronous code from the program will run. Next, any code in front of it in the line will run. This means it might be more than two seconds before delayedHello() is actually executed.
+
+Let’s look at how we’ll be using setTimeout() to construct asynchronous promises:
+*/
+const returnPromiseFunction = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('I resolved!');
+    }, 1000);
+  });
+};
+
+const prom = returnPromiseFunction();
+/*
+In the example code, we invoked returnPromiseFunction() which returned a promise. We assigned that promise to the variable prom. Similar to the asynchronous promises you may encounter in production, prom will initially have a status of pending.
+
+Let’s explore setTimeout() a bit more.
+Instructions
+1.
+
+Create a function, usingSTO(). Your usingSTO() function should have no parameters. Inside the function body, it should print a string to the console. This can be any string you want (as long as it’s not either “This is the first line of synchronous code.” or “This is the last line of synchronous code.”).
+
+Check your work to move on to the next step.
+2.
+
+Now, let’s invoke the setTimeout() function. Remember, setTimeout() has two parameters. Invoke setTimeout() with your usingSTO() function as the first argument and a number between 0 and 3000 as the second argument.
+3.
+
+Take a moment to predict the output of this program.
+
+Whenever you’re ready, type node app.js in the terminal and hit enter.
+
+See if the program’s output in the terminal lines up with what you expected.
+
+Be sure to click “Check Work” after running the command to pass this checkpoint and move on to the next exercise.
+
+
+*/
