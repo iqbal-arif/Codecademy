b8bc6d83ffd14a687bbd4e37a4be5af3d6de1e15
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-12 22:37:02 -0600

    modified:   JavaScript/Notes/Intermediate/JS_Asynchronous.js

--- JavaScript/Notes/Intermediate/JS_Asynchronous.js
+++ JavaScript/Notes/Intermediate/JS_Asynchronous.js
@@ -607,7 +607,7 @@ checkInventory3(order2)
 /*****************************************************/
 /*****************************************************/
 
-  /*
+/*
   *****************************
   9. Avoiding Common Mistakes
   *****************************
@@ -615,13 +615,11 @@ Promise composition allows for much more readable code than the nested callback 
 
 Mistake 1: Nesting promises instead of chaining them.
 */
-returnsFirstPromise()
-.then((firstResolveVal) => {
-  return returnsSecondValue(firstResolveVal)
-    .then((secondResolveVal) => {
-      console.log(secondResolveVal);
-    })
-})
+returnsFirstPromise().then((firstResolveVal) => {
+  return returnsSecondValue(firstResolveVal).then((secondResolveVal) => {
+    console.log(secondResolveVal);
+  });
+});
 /*
 Let’s break down what’s happening in the above code:
 
@@ -634,23 +632,28 @@ Let’s break down what’s happening in the above code:
 Instead of having a clean chain of promises, we’ve nested the logic for one inside the logic of the other. Imagine if we were handling five or ten promises!
 
 Mistake 2: Forgetting to return a promise.
+*/
 
 returnsFirstPromise()
-.then((firstResolveVal) => {
-  returnsSecondValue(firstResolveVal)
-})
-.then((someVal) => {
-  console.log(someVal);
-})
-
+  .then((firstResolveVal) => {
+    returnsSecondValue(firstResolveVal);
+  })
+  .then((someVal) => {
+    console.log(someVal);
+  });
+/*
 Let’s break down what’s happening in the example:
 
-    We invoke returnsFirstPromise() which returns a promise.
-    We invoke .then() with a success handler.
-    Inside the success handler, we create our second promise, but we forget to return it!
-    We invoke a second .then(). It’s supposed to handle the logic for the second promise, but since we didn’t return, this .then() is invoked on a promise with the same settled value as the original promise!
+    1. We invoke returnsFirstPromise() which returns a promise.
+    2. We invoke .then() with a success handler.
+    3. Inside the success handler, we create our second promise, but we forget to return it!
+    4. We invoke a second .then(). It’s supposed to handle the logic for the second promise, but since we didn’t return, this .then() is invoked on a promise with the same settled value as the original promise!
 
 Since forgetting to return our promise won’t throw an error, this can be a really tricky thing to debug!
+
+/*****************************************************/
+/**********************PRACTICE***********************/
+/*****************************************************
 Instructions
 1.
 
@@ -667,3 +670,11 @@ Refactor, or rewrite, the code to avoid the two common mistakes: nesting and for
 Type node app.js in the terminal and hit enter to make sure your program is still working as expected.
 
 When you are ready to move on, press the “Check Work” button.
+*/
+checkInventory(order).then((resolvedValueArray) => {
+  return processPayment(resolvedValueArray).then((resolvedValueArray) => {
+    return shipOrder(resolvedValueArray).then((successMessage) => {
+      console.log(successMessage);
+    });
+  });
+});
