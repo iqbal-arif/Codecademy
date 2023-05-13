394aeb9a1b96edc46b268cc629dbcbbd3561396a
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-12 22:10:46 -0600

    modified:   JavaScript/Notes/Intermediate/JS_Asynchronous.js

--- JavaScript/Notes/Intermediate/JS_Asynchronous.js
+++ JavaScript/Notes/Intermediate/JS_Asynchronous.js
@@ -242,3 +242,127 @@ In the case of our dishwasher promise, the dishwasher will run then:
     The second handler, sometimes called onRejected, is a failure handler, and it should contain the logic for the promise rejecting.
 
 We can invoke .then() with one, both, or neither handler! This allows for flexibility, but it can also make for tricky debugging. If the appropriate handler is not provided, instead of throwing an error, .then() will just return a promise with the same settled value as the promise it was called on. One important feature of .then() is that it always returns a promise. We’ll return to this in more detail in a later exercise and explore why it’s so important.
+
+******************************************
+6. Success and Failure Callback Functions
+******************************************
+To handle a “successful” promise, or a promise that resolved, we invoke .then() on the promise, passing in a success handler callback function:
+*/
+const prom2 = new Promise((resolve, reject) => {
+  resolve('Yay!');
+});
+
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+
+prom2.then(handleSuccess); // Prints: 'Yay!'
+/*
+Let’s break down what’s happening in the example code:
+
+    prom is a promise which will resolve to 'Yay!'.
+    We define a function, handleSuccess(), which prints the argument passed to it.
+    We invoke prom‘s .then() function passing in our handleSuccess() function.
+    Since prom resolves, handleSuccess() is invoked with prom‘s resolved value, 'Yay', so 'Yay' is logged to the console.
+
+With typical promise consumption, we won’t know whether a promise will resolve or reject, so we’ll need to provide the logic for either case. We can pass both a success callback and a failure callback to .then().
+*/
+let prom3 = new Promise((resolve, reject) => {
+  let num = Math.random();
+  if (num < 0.5) {
+    resolve('Yay!');
+  } else {
+    reject('Ohhh noooo!');
+  }
+});
+
+const handleSuccess2 = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+
+const handleFailure2 = (rejectionReason) => {
+  console.log(rejectionReason);
+};
+
+prom3.then(handleSuccess2, handleFailure2);
+/*
+Let’s break down what’s happening in the example code:
+
+    prom is a promise which will randomly either resolve with 'Yay!' or reject with 'Ohhh noooo!'.
+    We pass two handler functions to .then(). The first will be invoked with 'Yay!' if the promise resolves, and the second will be invoked with 'Ohhh noooo!' if the promise rejects.
+
+    Note: The success callback is sometimes called the “success handler function” or the onFulfilled function. The failure callback is sometimes called the “failure handler function” or the onRejected function. 
+
+Let’s write some success and failure callbacks!
+*/
+/*****************************************************/
+/**********************PRACTICE***********************/
+/*****************************************************
+Instructions
+1.
+
+Take a look at the provided code in app.js. We use require() to include the function checkInventory() from library.js. It builds on the logic of the orderSunglasses() function you wrote in a previous exercise.
+
+    checkInventory() takes in an array representing an order and returns a promise.
+    If every item in the order is in stock, that promise resolves with the value "Thank you. Your order was successful."
+    Otherwise, the promise rejects with the value "We're sorry. Your order could not be completed because some items are sold out".
+
+We used setTimeout() to ensure that the checkInventory() promise settles asynchronously.
+
+If you’d like, look at the library.js file to see how it works. Press “Check Work” when you’re ready to move on.
+2.
+
+Write a function, handleSuccess(). You’ll use this function later on as your success handler. handleSuccess() should have one parameter, representing a resolved value. Inside the body of handleSuccess(), log the parameter to the console.
+3.
+
+Write a function, handleFailure(). You’ll use this function later on as your failure handler. handleFailure() should have one parameter, representing a rejection reason. Inside the body of handleFailure(), log the parameter to the console.
+4.
+
+Invoke checkInventory() with order. This will return a promise. Attach a .then() function to this. Pass into .then() the two handlers you wrote as callback functions.
+5.
+
+Type node app.js in the terminal and hit enter.
+
+/*library.js*/
+const inventory2 = {
+  sunglasses: 1900,
+  pants: 1088,
+  bags: 1344,
+};
+
+const checkInventory = (order) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let inStock = order.every((item) => inventory2[item[0]] >= item[1]);
+      if (inStock) {
+        resolve(`Thank you. Your order was successful.`);
+      } else {
+        reject(
+          `We're sorry. Your order could not be completed because some items are sold out.`
+        );
+      }
+    }, 1000);
+  });
+};
+
+module.exports = { checkInventory };
+
+/*app.js*/
+const { checkInventory } = require('./library.js');
+
+const order = [
+  ['sunglasses', 1],
+  ['bags', 2],
+];
+
+// Write your code below:
+
+handleSuccess = (resolve) => {
+  console.log(resolve);
+};
+
+const handleFailure = (reject) => {
+  console.log(reject);
+};
+
+checkInventory(order).then(handleSuccess, handleFailure);
