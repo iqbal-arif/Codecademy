31030126769639650bf630708b59e9bc6d4637b8
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-15 12:15:57 -0600

    modified:   JavaScript/Notes/Intermediate/Async_Await/JS_Async_Await.js

--- JavaScript/Notes/Intermediate/Async_Await/JS_Async_Await.js
+++ JavaScript/Notes/Intermediate/Async_Await/JS_Async_Await.js
@@ -7,3 +7,42 @@ Often in web development, we need to handle asynchronous actions— actions we c
 Originally, JavaScript used callback functions to handle asynchronous actions. The problem with callbacks is that they encourage complexly nested code which quickly becomes difficult to read, debug, and scale. With ES6, JavaScript integrated native promises which allow us to write significantly more readable code. JavaScript is continually improving, and ES8 provides a new syntax for handling our asynchronous action, async...await. The async...await syntax allows us to write asynchronous code that reads similarly to traditional synchronous, imperative programs.
 
 The async...await syntax is syntactic sugar— it doesn’t introduce new functionality into the language, but rather introduces a new syntax for using promises and generators. Both of these were already built in to the language. Despite this, async...await powerfully improves the readability and scalability of our code. Let’s learn how to use it!
+
+
+********************
+2. The async Keyword
+********************
+The async keyword is used to write functions that handle asynchronous actions. We wrap our asynchronous logic inside a function prepended with the async keyword. Then, we invoke that function.
+*/
+async function myFunc() {
+  // Function body here
+};
+ 
+myFunc();
+/*
+We’ll be using async function declarations throughout this lesson, but we can also create async function expressions:
+*/
+const myFunc = async () => {
+  // Function body here
+};
+ 
+myFunc();
+/*
+async functions always return a promise. This means we can use traditional promise syntax, like .then() and .catch with our async functions. An async function will return in one of three ways:
+
+    1. If there’s nothing returned from the function, it will return a promise with a resolved value of undefined.
+    2. If there’s a non-promise value returned from the function, it will return a promise resolved to that value.
+    3. If a promise is returned from the function, it will simply return that promise
+*/
+async function fivePromise() { 
+  return 5;
+}
+ 
+fivePromise()
+.then(resolvedValue => {
+    console.log(resolvedValue);
+  })  // Prints 5
+/*
+In the example above, even though we return 5 inside the function body, what’s actually returned when we invoke fivePromise() is a promise with a resolved value of 5.
+
+Let’s write an async function!
