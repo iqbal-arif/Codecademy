a6a99e02e4bbbd4d91404d6b2623f15c6341f007
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-15 21:42:23 -0600

    modified:   JavaScript/Notes/Intermediate/Async_Await/app_5.js

--- JavaScript/Notes/Intermediate/Async_Await/app_5.js
+++ JavaScript/Notes/Intermediate/Async_Await/app_5.js
@@ -1,3 +1,13 @@
 const cookBeanSouffle = require('./library.js');
 
 // Write your code below:
+
+async function hostDinnerParty() {
+  try {
+    let cooked = await cookBeanSouffle();
+    console.log(`${cooked} is served!`);
+  } catch (error) {
+    console.log(error);
+    console.log('Ordering a pizza!');
+  }
+}
