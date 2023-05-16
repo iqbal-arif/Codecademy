50e268aa27623f62e47ebce417df91691713f20c
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-15 22:17:02 -0600

    modified:   JavaScript/Notes/Intermediate/Async_Await/app_7.js

--- JavaScript/Notes/Intermediate/Async_Await/app_7.js
+++ JavaScript/Notes/Intermediate/Async_Await/app_7.js
@@ -6,3 +6,12 @@ let {
 } = require('./library.js');
 
 // Write your code below:
+
+async function serveDinner() {
+  const serveDinnerAgain = await Promise.all([
+    cookBeans(),
+    steamBroccoli(),
+    cookRice(),
+    bakeChicken(),
+  ]);
+}
