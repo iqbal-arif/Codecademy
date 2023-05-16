500c2cf2e54e35685e003774d1399f7ec9d9a835
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-16 12:01:20 -0600

    modified:   JavaScript/Notes/Intermediate/Async_Await/JS_Async_Await.js

--- JavaScript/Notes/Intermediate/Async_Await/JS_Async_Await.js
+++ JavaScript/Notes/Intermediate/Async_Await/JS_Async_Await.js
@@ -557,5 +557,39 @@ Make sure to click “Check Work” to complete this step.
 Code files also available in separate app_5.js & library_5.js
 */
 
+let {
+  cookBeans,
+  steamBroccoli,
+  cookRice,
+  bakeChicken,
+} = require('./library.js');
+
+// Write your code below:
+
+async function serveDinnerAgain() {
+  const foodArray = await Promise.all([
+    steamBroccoli(),
+    cookRice(),
+    bakeChicken(),
+    cookBeans(),
+  ]);
+  // let vege = foodArray[0];
+  // let grain = foodArray[1];
+  // let meat = foodArray[2];
+  // let beans = foodArray[3];
+
+  // cconsole.log(`Dinner is served. We're having ${vege}, ${grain}, ${meat}, and ${beans}.`);
+  let vege = foodArray[0];
+  let grain = foodArray[1];
+  let meat = foodArray[2];
+  let beans = foodArray[3];
+
+  console.log(
+    `Dinner is served. We're having ${vege}, ${grain}, ${meat}, and ${beans}.`
+  );
+}
+
+serveDinnerAgain();
+
 /*****************************************************/
 /*****************************************************/
