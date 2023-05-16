78e65200d0e083508431658aeff58356708f2d15
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-16 17:04:21 -0600

    new file:   JavaScript/Quizes/js_promise_async_await_qz.js

---
+++ JavaScript/Quizes/js_promise_async_await_qz.js
@@ -0,0 +1,14 @@
+/*
+Q1: Which of the following is NOT a benefit of the async...await syntax?
+A1: It causes promises to resolve faster.
+it reads more similarly to synchronous code.
+it lets us catch rejected promise errors with the exact same syntax we use to catch other types of errors.
+it lets us easily store and refer back to the resolved values of earlier promises in a promise chain.
+*/
+
+/*
+Q2: What does the function example() return?
+
+async function example() { }
+A2: A promise
+*/
