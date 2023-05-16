5ef2419acdb40fa9b5acf9971f2084b31de1df58
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-15 22:30:49 -0600

    modified:   JavaScript/Notes/Intermediate/Async_Await/app_7.js

--- JavaScript/Notes/Intermediate/Async_Await/app_7.js
+++ JavaScript/Notes/Intermediate/Async_Await/app_7.js
@@ -1,17 +1,20 @@
 let {
-  cookBeans,
-  steamBroccoli,
-  cookRice,
-  bakeChicken,
-} = require('./library.js');
-
-// Write your code below:
-
-async function serveDinner() {
-  const serveDinnerAgain = await Promise.all([
-    cookBeans(),
-    steamBroccoli(),
-    cookRice(),
-    bakeChicken(),
-  ]);
-}
+    cookBeans,
+    steamBroccoli,
+    cookRice,
+    bakeChicken,
+  } = require("./library.js");
+  
+  // Write your code below:
+  
+  async function serveDinnerAgain() {
+    const foodArray = await Promise.all([
+      steamBroccoli(),
+      cookRice(),
+      bakeChicken(),
+     cookBeans(),
+    ]);
+    for (let i = 0; i < foodArray.length; i++) {
+      console.log(`Dinner is served. We’re having [first item in foodArray ], [second item in foodArray ], [third item in foodArray], and [fourth item in foodArray ]`foodArray[i]);
+    }
+  }
