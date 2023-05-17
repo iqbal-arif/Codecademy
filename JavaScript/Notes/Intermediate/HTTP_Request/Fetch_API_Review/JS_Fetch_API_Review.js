511af37003e0290de3040c77563a9b49834c7684
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-17 13:36:54 -0600

    new file:   JavaScript/Notes/Intermediate/HTTP_Request/Fetch_API_Review/JS_Fetch_API_Review.js

---
+++ JavaScript/Notes/Intermediate/HTTP_Request/Fetch_API_Review/JS_Fetch_API_Review.js
@@ -0,0 +1,28 @@
+/*
+***********
+1. Review
+***********
+In this lesson, we learned how to make GET and POST requests using the Fetch API and async/await keywords. Let’s recap on the concepts covered in the previous exercises:
+
+    1. GET and POST requests can be created in a variety of ways.
+    2. We can use fetch() and async/await to asynchronous request data from APIs.
+    3. Promises are a type of JavaScript object that represents data that will eventually be returned from a request.
+    4. The fetch() function can be used to create requests and will return promises.
+    5. We can chain .then() methods to handle promises returned by the fetch() function.
+    6. The async keyword is used to create asynchronous functions that will return promises.
+    7. The await keyword can only be used with functions declared with the async keyword.
+    8. The await keyword suspends the program while waiting for a promise to resolve.
+
+Congratulations on learning all about asynchronous requests using fetch(), async, and await! These concepts are fundamental to helping you develop more robust web apps!
+Instructions
+
+Play around with the browser and code to make GET and POST requests. To make POST requests to the Rebrandly API, make sure you assign apiKey your Rebrandly API key and run the code!
+
+If you want to challenge yourself:
+
+    1. Rewrite the requests from scratch.
+    2. Replace the helper functions (renderResponse() in public/helperFunctions.js) with your own code.
+    3. Use different APIs to make GET/POST requests using async/await.
+
+
+*/
