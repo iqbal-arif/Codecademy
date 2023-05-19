552d57f78f2c6fc4fb059ed7d4d49d11116c57e4
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-18 19:09:55 -0600

    new file:   Back_End_Engineer/Challenges/agreeOrDisagree.js

---
+++ Back_End_Engineer/Challenges/agreeOrDisagree.js
@@ -0,0 +1,18 @@
+/*
+agreeOrDisagree()
+Instructions
+1.
+
+Write a function, agreeOrDisagree(), that takes in two strings, and returns 'You agree!' if the two strings are the same and 'You disagree!' if the two strings are different.
+
+*/
+// Write your function here:
+
+const agreeOrDisagree = (stringOne, stringTwo) =>
+  stringOne === stringTwo ? 'You agree!' : 'You disagree!';
+
+// Uncomment the line below when you're ready to try out your function
+// console.log(agreeOrDisagree("yep", "yep"))
+// Should print 'You agree!'
+
+// We encourage you to add more function call of your own to test your code!
