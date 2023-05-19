4cd697fa53e7d583927f3eef6bf12f6b34b5a46d
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-18 19:01:57 -0600

    modified:   Back_End_Engineer/Challenges/fixTheBrokenCode.js new file:   Back_End_Engineer/Challenges/reportingForDuty.js

--- Back_End_Engineer/Challenges/fixTheBrokenCode.js
+++ Back_End_Engineer/Challenges/fixTheBrokenCode.js
@@ -1,5 +1,7 @@
 /*Fix The Broken Code
-Instructions
+**************************
+*******Instructions*******
+**************************
 1.
 
 We wrote a function, rollTheDice(), which is supposed to simulate two dice being rolled and totalled. It’s close to doing what we want, but there’s something not quite right. Can you fix our code, please?
