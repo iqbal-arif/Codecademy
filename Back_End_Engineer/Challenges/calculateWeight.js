c5e9ba2e7d664a5e2f0261b29f22dedde186083e
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-18 18:56:13 -0600

    modified:   Back_End_Engineer/Challenges/calculateWeight.js

--- Back_End_Engineer/Challenges/calculateWeight.js
+++ Back_End_Engineer/Challenges/calculateWeight.js
@@ -25,3 +25,43 @@ Handle the following cases:
 For all other inputs, return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.'
 
 */
+// Write your function here:
+
+const calculateWeight = (earthWeight, planet) => {
+  console.log(earthWeight);
+  console.log(planet);
+  if (
+    planet === 'Mercury' ||
+    planet === 'Venus' ||
+    planet === 'Mars' ||
+    planet === 'Jupiter' ||
+    planet === 'Saturn'
+  ) {
+    let weight;
+    switch (planet) {
+      case 'Mercury':
+        return earthWeight * 0.378;
+        break;
+      case 'Venus':
+        return earthWeight * 0.907;
+        break;
+      case 'Mars':
+        return earthWeight * 0.377;
+        break;
+      case 'Jupiter':
+        return earthWeight * 2.36;
+        break;
+      case 'Saturn':
+        return earthWeight * 0.916;
+        break;
+      default:
+    }
+  } else {
+    return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.';
+  }
+};
+
+// Uncomment the line below when you're ready to try out your function
+console.log(calculateWeight(100, 'Jupiter')); // Should print 236
+
+// We encourage you to add more function calls of your own to test your code!
