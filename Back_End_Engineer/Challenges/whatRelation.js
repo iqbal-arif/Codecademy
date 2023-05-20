bab65060ca7609baf91847250588d6285779349d
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-20 09:51:21 -0600

    modified:   Back_End_Engineer/Challenges/whatRelation.js

--- Back_End_Engineer/Challenges/whatRelation.js
+++ Back_End_Engineer/Challenges/whatRelation.js
@@ -36,11 +36,14 @@ const whatRelation = (percentSharedDNA) => {
   if (percentSharedDNA === 100) {
     return 'You are likely identical twins.';
   }
-  if (percentSharedDNA > 34) {
+  if (percentSharedDNA >= 99) {
     return 'You are likely parent and child or full siblings.';
   }
-  if (percentSharedDNA < 13) {
+  if (percentSharedDNA >= 34) {
     return 'You are likely grandparent and grandchild, aunt/uncle and niece/nephew, or half siblings.';
+  }
+  if (percentSharedDNA < 13) {
+    return 'You are likely parent and child or full siblings.';
   }
   if (percentSharedDNA > 5) {
     return 'You are likely 1st cousins.';
