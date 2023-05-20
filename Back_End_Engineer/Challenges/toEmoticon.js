c9fcc3ed6d1c2d5e2802c752e90d5670a1418c90
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-20 10:33:23 -0600

    modified:   Back_End_Engineer/Challenges/toEmoticon.js

--- Back_End_Engineer/Challenges/toEmoticon.js
+++ Back_End_Engineer/Challenges/toEmoticon.js
@@ -38,7 +38,12 @@ const toEmoticon = (stringEmoticon) => {
   }
 };
 // Uncomment the line below when you're ready to try out your function
-// console.log(toEmoticon("whatever"))
+console.log(toEmoticon('whatever'));
 // Should print  '|_(* ~ *)_|'
 
 // We encourage you to add more function calls of your own to test your code!
+console.log(toEmoticon('shrug'));
+console.log(toEmoticon('smiley face'));
+console.log(toEmoticon('smiley face'));
+console.log(toEmoticon('winky face'));
+console.log(toEmoticon('heart'));
