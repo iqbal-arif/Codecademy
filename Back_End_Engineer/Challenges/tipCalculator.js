6cf787f1dc379a59d66b6db4cd49a7de194115ac
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-20 10:15:37 -0600

    modified:   Back_End_Engineer/Challenges/tipCalculator.js

---
+++ Back_End_Engineer/Challenges/tipCalculator.js
@@ -0,0 +1,24 @@
+/*
+
+tipCalculator()
+Instructions
+1.
+
+Create a function, tipCalculator(), that has two parameters, a string representing the quality of the service received and a number representing the total cost.
+
+Return the tip, as a number, based on the following:
+‘bad’ should return a 5% tip
+‘ok’ should return a 15% tip
+‘good’ should return a 20% tip
+‘excellent’ should return a 30% tip
+all other inputs should default to 18%
+
+tipCalculator('good', 100) // Should return 20
+*/
+
+// Write your function here:
+
+// Uncomment the line below when you're ready to try out your function
+// console.log(tipCalculator('good', 100)) //should return 20
+
+// We encourage you to add more function calls of your own to test your code!
