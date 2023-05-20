feb5ad2b611d4985cf94c52cab3db5a22863052b
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-20 10:26:45 -0600

    new file:   Back_End_Engineer/Challenges/toEmoticon.js

---
+++ Back_End_Engineer/Challenges/toEmoticon.js
@@ -0,0 +1,23 @@
+/*
+toEmoticon()
+Instructions
+1.
+
+Write a function, toEmoticon(), that takes in a string and returns the corresponding emoticon as a string. Use a switch/case, and cover these cases:
+
+'shrug' should return '|_{"}_|'
+'smiley face' should return ':)'
+'frowny face' should return':('
+'winky face' should return ';)'
+'heart' should return '<3'
+any other input should return '|_(* ~ *)_|'
+
+*/
+
+// Write your function here:
+
+// Uncomment the line below when you're ready to try out your function
+// console.log(toEmoticon("whatever"))
+// Should print  '|_(* ~ *)_|'
+
+// We encourage you to add more function calls of your own to test your code!
