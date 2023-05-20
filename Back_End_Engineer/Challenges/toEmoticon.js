a494cfb2b76c7e7e9eb2604c25af7917dc93fce3
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-20 10:31:19 -0600

    modified:   Back_End_Engineer/Challenges/toEmoticon.js

--- Back_End_Engineer/Challenges/toEmoticon.js
+++ Back_End_Engineer/Challenges/toEmoticon.js
@@ -15,7 +15,28 @@ any other input should return '|_(* ~ *)_|'
 */
 
 // Write your function here:
+const toEmoticon = (stringEmoticon) => {
+  switch (stringEmoticon) {
+    case 'shrug':
+      return '|_{"}_|';
+      break;
+    case 'smiley face':
+      return ':)';
+      break;
+    case 'frowny face':
+      return ':(';
+      break;
+    case 'winky face':
+      return ';)';
+      break;
+    case 'heart':
+      return '<3';
+      break;
 
+    default:
+      return '|_(* ~ *)_|';
+  }
+};
 // Uncomment the line below when you're ready to try out your function
 // console.log(toEmoticon("whatever"))
 // Should print  '|_(* ~ *)_|'
