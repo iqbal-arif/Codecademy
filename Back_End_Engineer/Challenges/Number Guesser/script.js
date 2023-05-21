4b5a0fb88d0dd5d6398818e395d6d39b21fcd0bf
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-20 21:21:11 -0600

    modified:   Back_End_Engineer/Challenges/Number Guesser/script.js

--- Back_End_Engineer/Challenges/Number Guesser/script.js
+++ Back_End_Engineer/Challenges/Number Guesser/script.js
@@ -4,3 +4,15 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+};
+
+
+const compareGuesses = (humanGuess,computerGuess,secretTarget) =>{
+    secretTarget = 9;
+    humanGuess = 10;
+    computerGuesses = 8
+
+    if (humanGuess =)
+}
