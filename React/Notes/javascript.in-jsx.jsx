563a7641311e4637b58513ddc6d63da6a1b56c0f
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-06-28 22:57:20 +0500

    modified:   React/Notes/javascript.in-jsx.jsx

--- React/Notes/javascript.in-jsx.jsx
+++ React/Notes/javascript.in-jsx.jsx
@@ -1,5 +1,5 @@
 /*
-JavaScript In Your JSX In Your JavaScript
+1. JavaScript In Your JSX In Your JavaScript
 
 So far, we’ve focused on writing JSX expressions. It’s similar to writing bits of HTML, but inside of a JavaScript file.
 
@@ -20,3 +20,37 @@ const container = document.getElementById('app');
 const root = createRoot(container);
 // Write code here:
 root.render(<h1>{2 + 3}</h1>);
+
+/*
+2. 20 Digits of Pi in JSX
+
+You can now inject regular JavaScript into JSX expressions! This will be extremely useful.
+
+In the code editor, you can see a JSX expression that displays the first twenty digits of pi.
+
+Study the expression and notice the following:
+
+    1. The code is written in a JavaScript file. By default, it will all be treated as regular JavaScript.
+    2. Find <div> on line 5. From there, up through </div>, the code will be treated as JSX.
+    3. Find Math. From there, up through (20), the code will be treated as regular JavaScript again.
+    4. The curly braces themselves won’t be treated as JSX or as JavaScript. They are markers that signal the beginning and end of a JavaScript injection into JSX, similar to the quotation marks that signal the boundaries of a string.
+
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const pi = (
+  <div>
+    <h1>
+      PI, YALL!
+    </h1>
+    <p>
+      {Math.PI.toFixed(20)}
+    </p>
+  </div>
+);
+
+ReactDOM.render(
+	pi,
+	document.getElementById('app')
+);
