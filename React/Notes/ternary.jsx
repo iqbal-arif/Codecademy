de95068fa9e4c8525cd6c7a0ce91ef7a8c54d18d
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-06-30 23:19:14 +0500

    modified:   React/Notes/ternary.jsx

--- React/Notes/ternary.jsx
+++ React/Notes/ternary.jsx
@@ -23,21 +23,21 @@ Take a look at app.js.
 
 On line 16, replace x, y, and z so that the expression returns 'kitty' if coinToss() is 'heads' and 'doggy' if coinToss() is 'tails'.
 */
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 const root = createRoot(container);
-function coinToss () {
+function coinToss() {
   // Randomly return either 'heads' or 'tails'.
-  return Math.random() < 0.5 ? 'heads' : 'tails';
+  return Math.random() < 0.5 ? "heads" : "tails";
 }
 
 const pics = {
-  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
-  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+  kitty: "https://content.codecademy.com/courses/React/react_photo-kitty.jpg",
+  doggy: "https://content.codecademy.com/courses/React/react_photo-puppy.jpeg",
 };
 
-const img = <img src={coinToss()==="heads" ? pics.kitty: pics.doggy} />;
+const img = <img src={pics[coinToss() === "heads" ? "kitty" : "doggy"]}/>;
 
 root.render(img);
