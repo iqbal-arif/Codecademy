5725631bab35b391bc0b4c4ec0ca5f91a738d1bf
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-02 18:22:02 +0500

    modified:   React/Projects/Animal Fun Facts/app.js

--- React/Projects/Animal Fun Facts/app.js
+++ React/Projects/Animal Fun Facts/app.js
@@ -1,1 +1,7 @@
 import { animals } from './animals';
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app'); 
+const root = createRoot(container);
