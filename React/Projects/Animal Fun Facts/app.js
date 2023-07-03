55d342c5b698968e0e13368c752abac7f0c8343d
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-03 23:41:02 +0500

    modified:   React/Projects/Animal Fun Facts/app.js

--- React/Projects/Animal Fun Facts/app.js
+++ React/Projects/Animal Fun Facts/app.js
@@ -1,7 +1,18 @@
-import { animals } from './animals';
+import { animals } from "./animals";
 
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('app'); 
+const container = document.getElementById("app");
 const root = createRoot(container);
+
+const title = " ";
+const background = (
+  <img className="background" src="./images/ocean.jpg" alt="ocean" />
+);
+const animalFacts = (
+  <h1>{title === " " ? "Click an animal for a fun fact !" : title}</h1>;
+  <div>{background}</div>
+);
+
+root.render(animalFacts);
