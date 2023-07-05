6dec9472382a07f0976ee6f12c55f3544bd56b6c
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-05 14:14:13 +0500

    modified:   React/Projects/Animal Fun Facts/app.js

--- React/Projects/Animal Fun Facts/app.js
+++ React/Projects/Animal Fun Facts/app.js
@@ -10,6 +10,22 @@ const title = " ";
 const background = (
   <img className="background" src="./images/ocean.jpg" alt="ocean" />
 );
+
+const image = animals.forEach(animal => console.log(animal))
+const images = [];
+for (const animal in animals) {
+  images.push(
+    <img
+      key={animal}
+      className="animal"
+      src={animals[animal].image}
+      alt={animal}
+      aria-label={animal}
+      role="button"
+    />
+  );
+}
+
 const animalFacts = (
   <div>
     <h1>{title === " " ? "Click an animal for a fun fact !" : title}</h1>;
