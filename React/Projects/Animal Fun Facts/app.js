c1a74fbbfccd58ddcd97922e829f1b990d5d44a3
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-27 00:08:45 +0500

    modified:   React/Projects/Animal Fun Facts/app.js

--- React/Projects/Animal Fun Facts/app.js
+++ React/Projects/Animal Fun Facts/app.js
@@ -7,53 +7,43 @@ const container = document.getElementById("app");
 const root = createRoot(container);
 
 const title = " ";
+
 const background = (
   <img className="background" src="./images/ocean.jpg" alt="ocean" />
 );
+
+function displayFact(e) {
+  const clickedAnimal = e.target.alt;
+  const randomIndex = Math.floor(
+    Math.random() * animals[clickedAnimal].facts.length
+  );
+  const animalFunFacts = animals[clickedAnimal].facts[randomIndex];
+  const p = document.getElementById("fact");
+  p.innerHTML = animalFunFacts;
+}
+
 const images = [];
 for (const animal in animals) {
   images.push(
     <img
-      onClick={displayFact}
       key={animal}
       className="animal"
       src={animals[animal].image}
       alt={animal}
       aria-label={animal}
       role="button"
+      onClick={displayFact}
     />
   );
 }
 
-const displayFact = (e) => {
-    if (animals === "dolphin"){
-        e.target.alt="dolphin"
-    }
-  const clickedAnimal = e.target.alt;
-  const randomFacts = Math.floor(Math.random() * clickedAnimal.fact.length);
-  const p = document.getElementById("fact")
-  return p.innerHTML=(randomFacts);
-};
-
-const randomFacts = Math.floor(Math.random() * (10));
-console.log(randomFacts)
-const displayFact = (e) => {
-    if (animals === "dolphin"){
-        e.target.alt="dolphin"
-    }
-}
-console.log(animals.dolphin.facts.length)
-
-console.log(animals.dolphin.image)
-
 const animalFacts = (
-    <div>
-      <div>
-        <h1>{title === " " ? "Click an animal for a fun fact !" : title}</h1>;
-        <img className="background" alt="ocean" src="/images/ocean.jpg" />
-      </div>
-      <div className="animals">{images}</div>
-      <p id="fact"></p>
-    </div>
-  );
-  root.render(animalFacts);
+  <div>
+    <h1>{title === " " ? "Click an animal for a fun fact !" : title}</h1>;
+    {background}
+    <p id="fact"></p>
+    <div className="animals">{images}</div>
+  </div>
+);
+
+root.render(animalFacts);
