c95e87d23dd99650a69f174e130c2d24cf6f5021
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-03 03:57:48 +0500

    modified:   React/Projects/2. Authorization Form/Contact.js

--- React/Projects/2. Authorization Form/Contact.js
+++ React/Projects/2. Authorization Form/Contact.js
@@ -1,28 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Contact() {
-  const password = 'swordfish';
-  const [authorized, setAuthorized] = useState(false);
+  const password = "swordfish";
+  const [authorized, setAuthorized] = useState(true);
 
   function handleSubmit(e) {
     const enteredPassword = e.target.querySelector(
-      'input[type="password"]').value;
+      'input[type="password"]'
+    ).value;
     const auth = enteredPassword == password;
-    setAuthorized(auth)
+    setAuthorized(auth);
   }
-
+  { authorized }
+    ? (h1Text = <h1>Contact</h1>)
+    : (h1Text = <h1>Enter the Password</h1>);
   return (
-      <div id="authorization">
-        <h1>Contact</h1>
-        <ul>
-          <li>
-            client@example.com
-          </li>
-          <li>
-            555.555.5555
-          </li>
-        </ul>
-      </div>
+    <div id="authorization">
+      <h1>{h1Text}</h1>
+      <ul>
+        <li>client@example.com</li>
+        <li>555.555.5555</li>
+      </ul>
+    </div>
   );
 }
 
