cbe12d201c8e1e120cb832114b844ec69e50e3e8
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-08 13:46:39 +0500

    renamed:    React/Notes/Components Interact/6. Access a Components props.md -> React/Notes/Components Interact/6. Access a Components props.js

--- React/Notes/Components Interact/6. Access a Components props.js
+++ React/Notes/Components Interact/6. Access a Components props.js
@@ -1,3 +1,4 @@
+/*
 1. Access a Component's props
 
 ---
@@ -8,9 +9,9 @@ A component’s props is an object. It holds information about that component.
 
 You’ve seen this before, but you might not have realized it! Let’s take a look at the HTML button tag. There are several pieces of information we can pass to the button tag, such as the type of the button.
 
-```
+*/
 <button type="submit" value="Submit"> Submit </button>
-```
+/*
 
 In this example, we’ve passed two pieces of information to the button tag, a type and a value. Depending on what type attribute we give to the <button> element, it will treat the form differently. In the same way, we can pass information to our own components to specify how they behave!
 
@@ -18,9 +19,9 @@ Props serve the same purpose for components as arguments do for functions.
 
 To access a component’s props object, you can reference the props object and the dot notation for its properties. Here’s an example:
 
-```
+*/
 props.name
-```
+/*
 
 This would retrieve the name property from the props object.
 
@@ -45,3 +46,30 @@ As always, App is exported to index.js and rendered.
 3. Click Run and think about what you see. If you’re seeing an empty object, you’re on the right track. The props object isn’t really empty. It has some properties that JSON.stringify() doesn’t detect. But even if you could see those properties, the props object still wouldn’t have much to show you right now.
 
 Keep going and let’s see what else we need.
+*/
+/**PropsDisplays.js**/
+import React from "react";
+
+function PropsDisplayer(props) {
+  const stringProps = JSON.stringify(props);
+  return (
+    <div>
+      <h1>CHECK OUT MY PROPS OBJECT</h1>
+      <h2>{stringProps}</h2>
+    </div>
+  );
+}
+
+export default PropsDisplayer;
+
+/**App**/
+import React from "react";
+import ReactDOM from "react-dom";
+
+import PropsDisplayer from "./PropsDisplayer";
+
+function App() {
+  return <PropsDisplayer />;
+}
+
+export default App;
