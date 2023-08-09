e1ac1406db999965f05cb0acc7e616099eebffbe
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-09 23:55:54 +0500

    modified:   React/Notes/Components Interact/15. props.children.js

--- React/Notes/Components Interact/15. props.children.js
+++ React/Notes/Components Interact/15. props.children.js
@@ -78,6 +78,24 @@ export default BigButton;
 
 /* App */
 
+import React from 'react';
+import List from './List';
+
+function App() {
+  return (
+    <div>
+      <List type='Living Musician'>
+        <li>Sachiko M</li>
+        <li>Harvey Sid Fisher</li>
+      </List>
+      <List type='Living Cat Musician'>
+        <li>Nora the Piano Cat</li>
+      </List>
+    </div>
+  );
+}
+
+export default App;
 
 /* List.js */
 import React from 'react';
