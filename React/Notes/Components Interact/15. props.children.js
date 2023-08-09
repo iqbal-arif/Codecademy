7bc21d7985cdcbb4986bc77926e45e129a888102
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-09 23:55:24 +0500

    modified:   React/Notes/Components Interact/15. props.children.js

--- React/Notes/Components Interact/15. props.children.js
+++ React/Notes/Components Interact/15. props.children.js
@@ -78,3 +78,21 @@ export default BigButton;
 
 /* App */
 
+
+/* List.js */
+import React from 'react';
+
+function List(props) {
+  let titleText = `Favorite ${props.type}`;
+  if (props.children instanceof Array) {
+    titleText += 's';
+  }
+  return (
+    <div>
+      <h1>{titleText}</h1>
+      <ul></ul>
+    </div>
+  );
+}
+
+export default List;
