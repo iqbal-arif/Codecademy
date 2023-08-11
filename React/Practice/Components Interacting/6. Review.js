8f4035255e9eb4b3da84843bd89cc4576d5da791
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-11 23:58:08 +0500

    modified:   React/Practice/Components Interacting/6. Review.js

--- React/Practice/Components Interacting/6. Review.js
+++ React/Practice/Components Interacting/6. Review.js
@@ -2,5 +2,12 @@
 Q1: What is true about a component’s return statement? 
 A1: The return statement can return a mix of HTML elements and custom React components.
 
-
+Q2: Which code block is using the object defaultProp properly? 
+A2: Excellent work! defaultProps can hold different types of values, try to see if you can figure out them all. 
 */
+function Baptiste(props){
+    render(){
+        return <p>{props.text}</p>
+    }
+}
+Baptiste.defaultProps = {text: 'Welcome'}
