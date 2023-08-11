ad81801bc9b6bfe8c8b8c8b656c7d76061fe0467
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-12 00:10:32 +0500

    modified:   React/Quizes/3. Component Interacting.js

--- React/Quizes/3. Component Interacting.js
+++ React/Quizes/3. Component Interacting.js
@@ -1,6 +1,6 @@
 /*Quiz: Components Interacting
 
-Complete the following code snippet so that the parent component passes “Hello from parent!” to the child component.
+Q1. Complete the following code snippet so that the parent component passes “Hello from parent!” to the child component.
 */
 function ParentComponent() {
   return (
@@ -12,4 +12,16 @@ function ParentComponent() {
  
 function ChildComponent(props) {
   return <p>{props.message}</p>;
+}
+
+/*
+Q2: How would you get an <Animal /> to render <h1>cat</h1>?
+A2: You Can't
+*/
+function Animal(props) {
+  if (props.type == 'cat') {
+    return <h1>Meow Meow</h1>;
+  } else if (props.type == 'dog') {
+    return <h1>Arf Arf</h1>;
+  }
 }
