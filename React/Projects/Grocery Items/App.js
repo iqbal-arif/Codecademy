c79a967d067dfdbb0a1d82be2d13f8d3cf7995ac
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-12 23:56:57 +0500

    modified:   React/Projects/Grocery Items/App.js

--- React/Projects/Grocery Items/App.js
+++ React/Projects/Grocery Items/App.js
@@ -7,3 +7,10 @@ function App() {
 }
 
 export default App;
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
