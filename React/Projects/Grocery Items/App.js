e9bc746bc5f65cde39d97765be238330ae417f49
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-13 23:44:38 +0500

    modified:   React/Projects/Grocery Items/App.js

--- React/Projects/Grocery Items/App.js
+++ React/Projects/Grocery Items/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import HelloWorld from "./HelloWorld";
+import GroceryItem from "./GroceryItem";
+
+import React from "react";
+import GroceryItem from "./GroceryItem";
 
 function App() {
-  return <HelloWorld />;
+  return (
+    <div>
+      <GroceryItem item="Eggs" />
+      <GroceryItem item="Banana" />
+      <GroceryItem item="Strawberry" />
+      <GroceryItem item="Bread" />
+      <GroceryItem />;<p>This is Grocery List</p>
+    </div>
+  );
 }
 
 export default App;
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("app")).render(<App />);
