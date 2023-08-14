166853fba777413f0e16521872bb02104f08dfe5
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-14 12:49:50 +0500

    modified:   React/Projects/3. CodeyOverflow Forum/Header.js

--- React/Projects/3. CodeyOverflow Forum/Header.js
+++ React/Projects/3. CodeyOverflow Forum/Header.js
@@ -1,9 +1,25 @@
 import React from "react";
 
 function Header(props) {
-  return( 
-    <h1>{props.username}r</h1>
-    <img src = {props.profileImg}/>);
+  return (
+    <div>
+      <h1>{props.username}r</h1>
+      <img src={props.profileImg} />
+    </div>
+  );
 }
 
 export default Header;
+
+// import React from 'react';
+
+// function Header (props) {
+//   return (
+//     <div>
+//     <img src={props.profileImg} alt=''/>
+//     <h1>{props.username}</h1>
+//     </div>
+//   )
+// }
+
+// export default Header;
