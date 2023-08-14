905abd2d47a321f4786da2e7bf6f984a5fe3391d
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-14 12:48:50 +0500

    modified:   React/Projects/3. CodeyOverflow Forum/Card.js

--- React/Projects/3. CodeyOverflow Forum/Card.js
+++ React/Projects/3. CodeyOverflow Forum/Card.js
@@ -2,13 +2,31 @@ import React from "react";
 import Header from "./Header";
 import Body from "./Body";
 
-function Card() {
+function Card(props) {
   return (
     <div>
-      <Header />;
-      <Body />;
+      <Header
+        profileImg={props.commentObject.profileImg}
+        username={props.commentObject.username}
+      />
+      <Body comment={props.commentObject.comment} />
     </div>
   );
 }
 
 export default Card;
+
+// import React from 'react';
+// import Header from './Header';
+// import Body from './Body';
+
+// function Card (props) {
+//   return (
+//     <div>
+//       <Header profileImg={props.commentObject.profileImg}
+//             username= {props.commentObject.username}/>
+//       <Body comment={props.commentObject.comment}/>
+//     </div>
+//   )
+// }
+// export default Card;
