d015d130e880b02880e31336dfec75671a0ec657
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-14 12:52:02 +0500

    modified:   React/Projects/3. CodeyOverflow Forum/index.js

--- React/Projects/3. CodeyOverflow Forum/index.js
+++ React/Projects/3. CodeyOverflow Forum/index.js
@@ -3,4 +3,19 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("app")).render(<App />);
+createRoot(document.getElementById("app")).render(<App />);
+
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+
+// import App from "./App";
+
+// ReactDOM.createRoot(document.getElementById("app")).render(<App />);
+
+// // import {StrictMode} from 'react';
+// import React from 'react';
+// // import ReactDOM from 'react-dom/client';
+// import {createRoot} from 'react-dom/client';
+
+// import App from './App';
+// ReactDOM.createRoot(document.getElementById('app')).render(<App/>);
