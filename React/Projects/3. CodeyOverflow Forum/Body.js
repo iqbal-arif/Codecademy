39d629778e1c1585c2d01daa8e9d348c51122266
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-14 12:50:56 +0500

    modified:   React/Projects/3. CodeyOverflow Forum/Body.js

--- React/Projects/3. CodeyOverflow Forum/Body.js
+++ React/Projects/3. CodeyOverflow Forum/Body.js
@@ -1,22 +1,27 @@
 import React from "react";
-import { comments } from "./commentData";
 
-const commentsList = [];
-for (const comment in comments) {
-  commentsList.push(
-    <img
-      className="animal"
-      src={comments[comment].profileImg}
-      alt={comment}
-      aria-label={comment}
-    />
-  );
-}
+// const commentsList = [];
+// for (const comment in comments) {
+//   commentsList.push(
+//     <img
+//       className="animal"
+//       src={comments[comment].profileImg}
+//       alt={comment}
+//       aria-label={comment}
+//     />
+//   );
+// }
 
-console.log({ commentsList });
+// console.log({ commentsList });
 
 function Body(props) {
   return <p>{props.comment}</p>;
 }
 
 export default Body;
+
+// import React from 'react';
+// function Body (props) {
+//   return <p>{props.comment}</p>
+// }
+// export default Body;
