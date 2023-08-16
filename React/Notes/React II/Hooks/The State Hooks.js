11612253ff9ae0bf7376972b80cce23f1bb926bb
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-16 22:00:33 +0500

    modified:   React/Notes/React II/Hooks/The State Hooks.js

--- React/Notes/React II/Hooks/The State Hooks.js
+++ React/Notes/React II/Hooks/The State Hooks.js
@@ -23,3 +23,97 @@ Instructions
 
 Press “Run” to see how the code behaves!
 */
+/* Index.js */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./Container/AppFunction";
+
+ReactDOM.createRoot(
+  document.querySelector("#app")
+).render(<App />);
+
+/* AppFunctions.js */
+
+import React, { useState } from "react";
+import NewTask from "../Presentational/NewTask";
+import TasksList from "../Presentational/TasksList";
+
+export default function AppFunction() {
+  const [newTask, setNewTask] = useState({});
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setNewTask((prev) => ({ ...prev, id: Date.now(), [name]: value }));
+  };
+
+  const [allTasks, setAllTasks] = useState([]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!newTask.title) return;
+    setAllTasks((prev) => [newTask, ...prev]);
+    setNewTask({});
+  };
+  const handleDelete = (taskIdToRemove) => {
+    setAllTasks((prev) => prev.filter((task) => task.id !== taskIdToRemove));
+  };
+
+  return (
+    <main>
+      <h1>Tasks</h1>
+      <NewTask
+        newTask={newTask}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+      <TasksList allTasks={allTasks} handleDelete={handleDelete} />
+    </main>
+  );
+}
+
+/* TaskList.js */
+
+import React from "react";
+
+export default function TasksList({ allTasks, handleDelete }) {
+  return (
+    <ul>
+      {allTasks.map(({ title, description, id }) => (
+        <li key={id}>
+          <div>
+            <h2>{title}</h2>
+            <button onClick={() => handleDelete(id)}>X</button>
+          </div>
+          {!description ? null : <p>{description}</p>}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+/* NewTask.js */
+
+import React from "react";
+
+export default function NewTask({ newTask, handleChange, handleSubmit }) {
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="title"
+        placeholder="New task"
+        value={newTask.title || ""}
+        onChange={handleChange}
+      />
+      {!newTask.title ? null : (
+        <>
+          <textarea
+            name="description"
+            placeholder="Details..."
+            value={newTask.description || ""}
+            onChange={handleChange}
+          />
+          <button type="submit">Add Task</button>
+        </>
+      )}
+    </form>
+  );
+}
