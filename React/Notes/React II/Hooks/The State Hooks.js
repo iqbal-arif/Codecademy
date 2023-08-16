95c3aed486f62fa4e77c4fe4dd6c5fb5d6ad0790
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-16 21:58:04 +0500

    new file:   React/Notes/React II/Hooks/The State Hooks.js

---
+++ React/Notes/React II/Hooks/The State Hooks.js
@@ -0,0 +1,25 @@
+/*
+Why Use Hooks?
+**************
+What should we do if we want to add a state to our function component? How about if we wanted our app to respond to the changes in data?
+
+In this lesson, we’ll learn about React Hooks and how they can help us powerfully wield function components.
+
+React Hooks, plainly put, are functions that let us manage the internal state of components and handle post-rendering side effects directly from our function components. Using Hooks, we can determine what we want to show the users by declaring how our user interface should look based on the state.
+
+React offers a number of built-in Hooks. A few of these include useState(), useEffect(), useContext(), useReducer(), and useRef(). See the full list in the React docs.
+
+In this lesson, we’ll learn how to:
+
+    1. Build a “stateful” function component.
+    2. Use the State Hook.
+    3. Initialize a state and set a state.
+    4. Define event handlers.
+    5. Use state setter callback functions.
+    6. Use state with arrays and objects.
+
+Instructions
+1. Open the AppFunction.js file. We will learn how this code works in the next few exercises. Don’t worry about the details of what is going on here just yet, but take a few moments to read through the definition of this function component and develop some theories about what this code may be doing.
+
+Press “Run” to see how the code behaves!
+*/
