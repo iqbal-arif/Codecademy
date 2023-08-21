7271b47d8352ab32853c8693c595af364a203ad1
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-21 23:52:01 +0500

    modified:   React/Notes/React II/Hooks/3. Initialize State.jsx

--- React/Notes/React II/Hooks/3. Initialize State.jsx
+++ React/Notes/React II/Hooks/3. Initialize State.jsx
@@ -44,28 +44,37 @@ Modify our current ColorPicker() component to initialize the state so that “To
 */
 /* ColorPickter.jsx*/
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const colorNames = ['Aquamarine', 'BlueViolet', 'Chartreuse', 'CornflowerBlue', 'Thistle', 'SpringGreen', 'SaddleBrown', 'PapayaWhip', 'MistyRose'];
+const colorNames = [
+  "Aquamarine",
+  "BlueViolet",
+  "Chartreuse",
+  "CornflowerBlue",
+  "Thistle",
+  "SpringGreen",
+  "SaddleBrown",
+  "PapayaWhip",
+  "MistyRose",
+];
 
 export default function ColorPicker() {
-  const [color, setColor] = useState();
+  const [color, setColor] = useState("Tomato");
 
- const divStyle = {backgroundColor: color};
+  const divStyle = { backgroundColor: color };
 
   return (
     <div style={divStyle}>
       <p>Selected color: {color}</p>
-      {colorNames.map((colorName)=>(
-        <button 
-          onClick={() => setColor(colorName)} 
-          key={colorName}>
-       	     {colorName}
-      	</button>
+      {colorNames.map((colorName) => (
+        <button onClick={() => setColor(colorName)} key={colorName}>
+          {colorName}
+        </button>
       ))}
     </div>
   );
 }
+
 
 
 
