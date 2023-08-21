b2a344eddf44abaf076141246ef16ebd16fc3356
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-08-22 00:45:13 +0500

    modified:   React/Notes/React II/Hooks/4.Use State Setter Outside JSX.jsx

--- React/Notes/React II/Hooks/4.Use State Setter Outside JSX.jsx
+++ React/Notes/React II/Hooks/4.Use State Setter Outside JSX.jsx
@@ -63,3 +63,30 @@ We also want to detect any changes to the input tag while the user types so add 
 
 In the if-statement of the handleChange() function, update the state using the state setter function setPhone() and set it to the new value newPhone.
 */
+
+// import React from "react";
+import React, { useState } from "react";
+
+// regex to match numbers between 1 and 10 digits long
+const validPhoneNumber = /^\d{1,10}$/;
+
+export default function PhoneNumber() {
+  // declare current state and state setter
+  const [phone, setPhone] = useState("");
+  const handleChange = ({ target }) => {
+    const newPhone = target.value;
+    const isValid = validPhoneNumber.test(newPhone);
+    if (isValid) {
+      // update state
+      setPhone(newPhone);
+    }
+    // just ignore the event, when new value is invalid
+  };
+
+  return (
+    <div className="phone">
+      <label for="phone-input">Phone: </label>
+      <input value={phone} id="phone-input" onChange={handleChange} />
+    </div>
+  );
+}
