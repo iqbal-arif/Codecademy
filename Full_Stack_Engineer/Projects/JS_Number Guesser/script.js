6840b488fed0401dbd3e66141c32988ce04342c2
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-21 21:35:04 -0600

    modified:   Full_Stack_Engineer/Projects/JS_Number Guesser/script.js

--- Full_Stack_Engineer/Projects/JS_Number Guesser/script.js
+++ Full_Stack_Engineer/Projects/JS_Number Guesser/script.js
@@ -8,8 +8,20 @@ function generateTarget() {
   return Math.floor(Math.random() * 10);
 }
 
+function guessAlter(guessRange) {
+  if (guessRange <= 0 || guessRange >= 10) {
+    alert('Your number is out of Range!!!. Please enter between 0 & 9');
+  } else {
+    compareGuesses();
+  }
+}
+
+console.log(guessAlter(15));
+
 function getAbsoluteDistance(userGuess, computerGuess) {
   let guess;
+  let target;
+  guessAlter(userGuess);
   if (guess === userGuess) {
     return Math.abs(userGuess - target);
   } else if (guess === computerGuess) {
@@ -23,7 +35,7 @@ function compareGuesses(userGuess, computerGuess, target) {
 
   return absoluteUserScore >= absoluteComputerScore ? true : false;
 }
-console.log(compareGuesses(2, 2, 1));
+// console.log(compareGuesses(2, 2, 1));
 
 function updateScore(gameResult) {
   gameResult === 'human' ? (humanScore = +1) : (computerScore = +1);
