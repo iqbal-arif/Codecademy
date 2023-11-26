a5399ef4caa860e1ffd3458e8c0b552738fb7c14
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-26 13:56:31 -0600

    new file:   JavaScript/Practice/JS_IN_Fix_the_code.js

---
+++ JavaScript/Practice/JS_IN_Fix_the_code.js
@@ -0,0 +1,27 @@
+/*
+******************
+1. Fix the broken Code
+******************
+
+We wrote a function, smallestPowerOfTwo(), which should take in an array and return the lowest power of 2 that is greater than or equal to each number in that array. A power of two is any value 2n where n is a non-negative integer (including 0). The list of powers of 2 for non-negative integers starts with [1, 2, 4, 8, 16, 32, 64,...] where 20=1, 21=2, 22=4, and so on
+*/
+const numbers = [5, 3, 9, 30];
+
+const smallestPowerOfTwo = (arr) => {
+  let results = [];
+  // The 'outer' for loop - loops through each element in the array
+  for (let i = 0; i < arr.length; i++) {
+    number = arr[i];
+
+    // The 'inner' while loop - searches for smallest power of 2 greater than the given number
+    i = 1;
+    while (i < number) {
+      i = i * 2;
+    }
+    results.push(i);
+  }
+  return results;
+};
+
+console.log(smallestPowerOfTwo(numbers));
+// Should print the returned array [ 8, 4, 16, 32 ] instead prints the returned array [8]
