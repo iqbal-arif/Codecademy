bc2c24307e443f9feda23b80cb751b4a9a176066
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-27 11:47:21 -0600

    modified:   Back_End_Engineer/Notes/Node _Core_Modules/1_Core_Modules.js

--- Back_End_Engineer/Notes/Node _Core_Modules/1_Core_Modules.js
+++ Back_End_Engineer/Notes/Node _Core_Modules/1_Core_Modules.js
@@ -234,3 +234,29 @@ getTrailDistancePromise('North Country')
   .catch((error) => {
     console.log('Trail not found!');
   });
+
+/*
+
+Review
+
+Awesome work! You’ve learned a lot about some fundamental Node.js concepts and modules. Let’s take a moment to review what we learned:
+
+    1. Node.js is a JavaScript runtime, an environment that allows us to execute our JavaScript code by converting it into something a computer can understand.
+
+    2. REPLs are processes that read, evaluate, print, and repeat (loop), and Node.js comes with its own REPL we can access in our terminal with the node command.
+    
+    3. We run JavaScript programs with Node in the terminal by typing node followed by the file name (if we’re in the same directory) or the absolute path of the file.
+    
+    4. Code can be organized into separate files, modules, and combined through requiring them where needed using the require() function.
+    
+    5. Core modules are built into the Node.js environment to efficiently perform common tasks.
+    
+    6. The console module exports a global console object allowing the terminal to act as a debugging console, similar to the JavaScript console object provided by web browsers.
+    
+    7. The process module is a global module that gives access to information about the Node.js runtime environment.
+    
+    8. The os module provides methods to retrieve information about the computer, operating system, and network interfaces.
+    
+    9. The util module contains methods used to maintain and debug your code.
+
+  */
