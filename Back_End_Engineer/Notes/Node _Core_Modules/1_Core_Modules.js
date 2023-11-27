1cdeff8cfd5d400e79b79a99f1ac8e7eeebe03d9
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-27 11:08:52 -0600

    modified:   Back_End_Engineer/Notes/Node _Core_Modules/1_Core_Modules.js

--- Back_End_Engineer/Notes/Node _Core_Modules/1_Core_Modules.js
+++ Back_End_Engineer/Notes/Node _Core_Modules/1_Core_Modules.js
@@ -27,3 +27,68 @@ console.log(petsArray);
 console.table(petsArray);
 
 console.assert(petsArray.length > 5);
+
+/*** 2. Process Module ***/
+/*
+The process.env property is an object which stores and controls information about the environment in which the process is currently running.
+
+We could store this information on the process.env.
+
+    One convention is to add a property to process.env with the key NODE_ENV and a value of either production or development.
+*/
+/*** i. process.env.NODE_ENV ***/
+
+if (process.env.NODE_ENV === 'development') {
+  console.log('Testing! Testing! Does everything work?');
+}
+
+/*** ii. process.memoryUsage() ***/
+/*
+The  returns information on the CPU demands of the current process.
+*/
+
+/*** iii. process.argv ***/
+/*
+The process.argv property holds an array of command line values provided when the current process was initiated. The first element in the array is the absolute path to Node, which ran the process. The second element in the array is the path to the file that’s running. The following elements will be any command line arguments provided when the process was initiated. Command line arguments are separated from one another with spaces
+console.log(process.argv[3]);
+*/
+
+/*** iv. Example of process.argv ***/
+/* 1.
+We want the program in app.js to store the starting amount of memory used (heapUsed), perform an operation, and then compare the final amount of memory used to the original amount. Right now, the initialMemory variable is assigned to null. Change this line, so that initialMemory is instead assigned the value of the heapUsed property on the object returned from invoking the process.memoryUsage() method
+
+2_
+We want the user of the program to be able to fill in their own word when they run the program. Right now word is assigned to null. Change the program so that when a user initiates the program with an additional command line argument, word will be assigned that value. For example, running the program with the command: node app.js Codecademy would result in word being assigned the value 'Codecademy'
+
+When we initiate the program with the command node app.js Codecademy, the first element in the process.argv array is the file path to Node, the second is the file path to the location program file, and the third is the string 'Codecademy'. In app.js, we’ll want to assign word to this third element in the array:
+
+let word = process.argv[2];
+
+3_
+3.
+
+Awesome! Now let’s run the program. Type node app.js followed by any word you like and then hit enter.
+
+node app.js Codecademy
+*/
+
+heapUsed;
+let word = process.argv[2];
+
+console.log(`Your word is ${word}`);
+
+// Create a new array
+let wordArray = [];
+
+// Loop 1000 times, pushing into the array each time
+for (let i = 0; i < 1000; i++) {
+  wordArray.push(`${word} count: ${i}`);
+}
+
+console.log(
+  `Starting memory usage: ${initialMemory}. \nCurrent memory usage: ${
+    process.memoryUsage().heapUsed
+  }. \nAfter using the loop to add elements to the array, the process is using ${
+    process.memoryUsage().heapUsed - initialMemory
+  } more bytes of memory.`
+);
