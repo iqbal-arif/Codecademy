ab481683ad733ff44cc94821ca71d13941a8f6c7
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-27 10:27:30 -0600

    modified:   Back_End_Engineer/Notes/Node _Core_Modules/1_Core_Modules.js

--- Back_End_Engineer/Notes/Node _Core_Modules/1_Core_Modules.js
+++ Back_End_Engineer/Notes/Node _Core_Modules/1_Core_Modules.js
@@ -7,4 +7,23 @@ const events = require('events');
 
 /*** 2. Accessing Node Core Module list */
 require('module').builtinModules;
-/*** 2. Node REPL */
+
+/****************** /*/ //*/*/*/*/*/*///*/ *************/
+/*** 1. Console Module ***/
+/*
+Node.js, the built-in console module exports a global console object that gives the terminal similar functionality.
+The console object provides many of the same familiar methods such as:
+
+    1_ .log() — to print messages to the terminal.
+    2_ .assert() — to print a message to the terminal if the value is falsy.
+    3_ .table() — to print out a table in the terminal from an object or array.
+Example
+*/
+const petsArray = ['dog', 'cat', 'bird', 'monkey'];
+
+// Add console methods below!
+console.log(petsArray);
+
+console.table(petsArray);
+
+console.assert(petsArray.length > 5);
