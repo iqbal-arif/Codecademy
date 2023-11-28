2a7da7170c7a169cb9417c6af2869b0556e5b18d
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-27 21:22:34 -0600

    modified:   Back_End_Engineer/Notes/Node_Essentials/7_Readable_Streams.js

--- Back_End_Engineer/Notes/Node_Essentials/7_Readable_Streams.js
+++ Back_End_Engineer/Notes/Node_Essentials/7_Readable_Streams.js
@@ -27,3 +27,35 @@ Let’s walk through the above code:
     5_ Our listener callback will log to the console 'The line read: [fileLine]', where [fileLine] is the line just read.
 
 */
+
+/****Shopping List Stream line Reading ***/
+
+/* 1. 
+You’re going to create a program that reads each item off of a shopping list (located in shoppingList.txt) and prints it to the console. Let’s take it one step at a time.
+
+Create a myInterface variable. Assign myInterface the value returned from invoking readline.createInterface().
+
+You’ll want to invoke readline.createInterface() with an object with a key of input and a value of fs.createReadStream(). Remember that fs.createReadStream() expects the file (as a string) from which it should read.
+
+2.
+Great work. Let’s create a listener callback function to use in the next step. Name this function printData. printData() should expect to receive some data (we named our parameter data) and it should log that data to the console in the format: Item: [data], where [data] is the argument passed into the function.
+
+3.
+
+We’re nearly there! Remember that a 'line' event will be emitted after each line from the file is read. Let’s assign our printData() function to execute whenever a 'line' event is emitted by using myInterface‘s .on() method.
+
+
+*/
+
+const readline = require('readline');
+const fs = require('fs');
+
+const myInterface2 = readline.createInterface({
+  input: fs.createReadStream('shoppingList.txt'),
+});
+
+const printData = (data) => {
+  console.log(`Item: ${data}`);
+};
+
+myInterface2.on('line', printData);
