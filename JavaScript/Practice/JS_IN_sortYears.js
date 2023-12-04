31f26974d3c4a9caff2712d2ba66fea2ff83bd2a
Arif Iqbal <arif_iqbal@yahoo.com>
2023-12-04 10:41:52 -0600

    modified:   JavaScript/Practice/JS_IN_sortYears.js

--- JavaScript/Practice/JS_IN_sortYears.js
+++ JavaScript/Practice/JS_IN_sortYears.js
@@ -9,11 +9,13 @@ You can test your function when you’re ready by passing in the years array or 
 */
 
 // Write your code here:
+const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922];
 
+const sortYears = (years) => years.sort();
+
+console.log(sortYears(years));
 // Feel free to uncomment the below code to test your function:
 /*
-const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
 
-console.log(sortYears(years))
 // Should print [ 2018, 2011, 1999, 1982, 1970, 1963, 1951, 1922 ]
 */
