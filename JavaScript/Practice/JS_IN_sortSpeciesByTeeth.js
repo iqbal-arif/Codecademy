ea75e66e46cf9d7572cae3be2c528e2707a67c10
Arif Iqbal <arif_iqbal@yahoo.com>
2023-12-11 11:35:06 -0600

    new file:   JavaScript/Practice/JS_IN_sortSpeciesByTeeth.js

---
+++ JavaScript/Practice/JS_IN_sortSpeciesByTeeth.js
@@ -0,0 +1,12 @@
+/*1. sortSpeciesByTeeth()
+
+Write a function sortSpeciesByTeeth() that takes in an array of species objects in the format:
+
+{speciesName: 'shark', numTeeth: 50 }
+
+and sorts the array in ascending order based on the average number of teeth that species possesses (numTeeth) .
+
+You’ll need to access each object’s .numTeeth property. Feel free to either write a named comparison function, or use an anonymous function for your argument to .sort().
+
+You can test your function when you’re ready by passing in the speciesArray array or by making your own!
+*/
