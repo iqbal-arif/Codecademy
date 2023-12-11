2613db077f26515e005769fad128d77cd3b0cc5c
Arif Iqbal <arif_iqbal@yahoo.com>
2023-12-11 12:46:32 -0600

    modified:   JavaScript/Practice/JS_IN_sortSpeciesByTeeth.js

--- JavaScript/Practice/JS_IN_sortSpeciesByTeeth.js
+++ JavaScript/Practice/JS_IN_sortSpeciesByTeeth.js
@@ -10,3 +10,27 @@ You’ll need to access each object’s .numTeeth property. Feel free to either 
 
 You can test your function when you’re ready by passing in the speciesArray array or by making your own!
 */
+const speciesArray = [
+  { speciesName: 'shark', numTeeth: 50 },
+  { speciesName: 'dog', numTeeth: 42 },
+  { speciesName: 'alligator', numTeeth: 80 },
+  { speciesName: 'human', numTeeth: 32 },
+];
+
+// Write your code here:
+
+const sortSpeciesByTeeth = (speciesArray) => {
+  return speciesArray.sort(
+    (firstItem, secondItem) => firstItem.numTeeth - secondItem.numTeeth
+  );
+};
+
+// Feel free to comment out the code below when you're ready to test your function!
+
+console.log(sortSpeciesByTeeth(speciesArray));
+
+// Should print:
+// [ { speciesName: 'human', numTeeth: 32 },
+//   { speciesName: 'dog', numTeeth: 42 },
+//   { speciesName: 'shark', numTeeth: 50 },
+//   { speciesName: 'alligator', numTeeth: 80 } ]
