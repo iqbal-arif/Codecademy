0fa308ad41b75f99844c856bdb47a8141a6beb22
Arif Iqbal <arif_iqbal@yahoo.com>
2023-12-12 13:57:51 -0600

    modified:   JavaScript/Practice/JS_IN_dogFactory.js

--- JavaScript/Practice/JS_IN_dogFactory.js
+++ JavaScript/Practice/JS_IN_dogFactory.js
@@ -15,49 +15,40 @@ dogFactory('Joe', 'Pug', 27)
 */
 // Write your code here:
 
-//Object Method
-
 const dogFactory = (name, breed, weight) => {
   return {
-    name,
-    breed,
-    weight,
-    getName() {
-      console.log(name);
+    _name: name,
+    _breed: breed,
+    _weight: weight,
+    get name() {
+      return this._name;
+    },
+    get breed() {
+      return this._breed;
+    },
+    get weight() {
+      return this._weight;
+    },
+
+    set name(newName) {
+      this._name = newName;
+    },
+    set breed(newBreed) {
+      this._breed = newBreed;
+    },
+    set weight(newWeight) {
+      this._weight = newWeight;
+    },
+    bark() {
+      return 'ruff! ruff!';
+    },
+    eatTooManyTreats() {
+      return (this._weight = this._weight + 1);
     },
   };
 };
 
 console.log(dogFactory('Joe', 'Pug', 27));
-dogFactory.getName;
-//Class Method
-/*
-class DogFactory {
-  constructor(name, breed, weight) {
-    this.name = name;
-    this.breed = breed;
-    this.weight = weight;
-  }
-  get name() {
-    // return (this._name = this.name);
-    return this.name;
-  }
-  get breed() {
-    // return (this._breed = this.breed);
-    return this.breed;
-  }
-  get weight() {
-    // return (this._weight = this.weight);
-    return this.weight;
-  }
-
-  set newName(){
-    return 
-  };
-}
-
-const dogFactory = new DogFactory('Joe', 'Pug', 27);
-console.log(dogFactory);
-
+dogFactory.name = 'Arif';
 console.log(dogFactory.name);
-*/
+console.log(dogFactory.bark);
