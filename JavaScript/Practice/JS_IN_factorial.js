af2d751f5e4828d25c72f7f5b67702a2fc33fea4
Arif Iqbal <arif_iqbal@yahoo.com>
2023-12-12 14:01:05 -0600

    new file:   JavaScript/Practice/JS_IN_factorial.js

---
+++ JavaScript/Practice/JS_IN_factorial.js
@@ -0,0 +1,12 @@
+/*
+Write a function factorial() that takes a number as an argument and returns the factorial of the number.
+
+Example:
+
+factorial(6); 
+// returns `720` because 6 * 5 * 4 * 3 * 2 * 1 = 720 
+
+Assume only positive numbers will be given as an argument for the factorial() function.
+s
+
+*/
