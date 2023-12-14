d4158a616730a04f87fdfeca89c5c5ece6062f22
Arif Iqbal <arif_iqbal@yahoo.com>
2023-12-13 23:49:15 -0600

    modified:   JavaScript/Practice/JS_IN_factorial.js

--- JavaScript/Practice/JS_IN_factorial.js
+++ JavaScript/Practice/JS_IN_factorial.js
@@ -7,6 +7,26 @@ factorial(6);
 // returns `720` because 6 * 5 * 4 * 3 * 2 * 1 = 720 
 
 Assume only positive numbers will be given as an argument for the factorial() function.
-s
-
+Factorial Formula
+n! = n × (n−1)!
 */
+const factorialMinusNum = (x) => {
+  let multipleX = 1;
+  let i = x;
+
+  do {
+    multipleX = multipleX * i;
+    i--;
+  } while (i > 0);
+  return multipleX;
+};
+
+const factorial = (x) => {
+  let absX = Math.abs(x);
+  let fM1 = absX - 1;
+
+  absX = absX * factorialMinusNum(fM1);
+  return absX;
+};
+
+console.log(factorial(4));
