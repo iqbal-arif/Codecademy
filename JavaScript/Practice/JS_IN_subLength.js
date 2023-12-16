90b60cb07d1eac33cf277397fcd622ebe9232b55
Arif Iqbal <arif_iqbal@yahoo.com>
2023-12-15 19:00:25 -0600

    modified:   JavaScript/Practice/JS_IN_subLength.js

--- JavaScript/Practice/JS_IN_subLength.js
+++ JavaScript/Practice/JS_IN_subLength.js
@@ -12,30 +12,28 @@ subLength('cheesecake', 'k'); // returns 0
 
 const subLength = (aString, aChar) => {
   //   console.log(aString, aChar);
-  const strLength = aString.length;
+  //   const strLength = aString.length;
   console.log(aString);
   let charCount = 0;
   for (let char of aString) {
-    if (char !== aChar) {
-      return 0;
-    } else {
-      charCount++;
-      if (charCount > 2 || charCount < 2) {
-        const firstCharIndex = aString.indexOf(char);
-        const lastCharIndex = aString.lastIndexOf(char);
-        const charSpan = lastCharIndex - firstCharIndex;
-        //   if (charCount === 1 || charCount > 3) {
-        //   charSpan === 0 || charSpan === 1 ? charSpan : charSpan + 1;
-        // }
-        //   return;
-        // } else {
-        //   // return charSpan;
-        // }
-        console.log(firstCharIndex);
-        console.log(lastCharIndex);
-        console.log(charSpan);
-        console.log(charCount);
-      }
+    console.log(char);
+
+    charCount++;
+    if (charCount <= 2) {
+      const firstCharIndex = aString.indexOf(char);
+      const lastCharIndex = aString.lastIndexOf(char);
+      const charSpan = lastCharIndex - firstCharIndex;
+      //   if (charCount === 1 || charCount > 3) {
+      //   charSpan === 0 || charSpan === 1 ? charSpan : charSpan + 1;
+      // }
+      //   return;
+      // } else {
+      //   // return charSpan;
+      // }
+      console.log(firstCharIndex);
+      console.log(lastCharIndex);
+      console.log(charSpan);
+      console.log(charCount);
     }
   }
 };
