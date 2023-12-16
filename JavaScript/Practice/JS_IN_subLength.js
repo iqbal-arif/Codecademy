49917a854e4f0452c6edf2d2a30d8929b1bf25e0
Arif Iqbal <arif_iqbal@yahoo.com>
2023-12-16 17:55:51 -0600

    modified:   JavaScript/Practice/JS_IN_subLength.js

--- JavaScript/Practice/JS_IN_subLength.js
+++ JavaScript/Practice/JS_IN_subLength.js
@@ -11,41 +11,47 @@ subLength('cheesecake', 'k'); // returns 0
 */
 
 const subLength = (aString, aChar) => {
+  console.log('includes', aString.includes(aChar));
   console.log(aString);
-  charIdentified = aChar;
-  charCount = 0;
-  for (let i = 0; i < aString.length; i++) {
-    if (aString.charAt(i) === charIdentified) {
-      charCount++;
-      console.log('charCount', charCount, aString.charAt(i));
+  const charCheck = aString.includes(aChar);
+  if (charCheck) {
+    charIdentified = aChar;
+    charCount = 0;
+    for (let i = 0; i < aString.length; i++) {
+      if (aString.charAt(i) === charIdentified) {
+        charCount++;
+        console.log('charCount', charCount, aString.charAt(i));
+      }
     }
-  }
-  console.log('charCount start', charCount);
-  if (charCount >= 3) {
-    return 0;
-  } else {
-    console.log(aString);
-    for (let char of aString) {
-      //   if (char !== aChar) {
-      //     return 0;
-      //   }
-      if (char === aChar) {
-        console.log('block', char);
-        const firstCharIndex = aString.indexOf(char);
-        const lastCharIndex = aString.lastIndexOf(char);
-        const charSpan = lastCharIndex - firstCharIndex;
-        console.log('charSpan block', charSpan);
-        if (charSpan === 0) {
-          return 0;
-        } else if (charSpan === 1 || charSpan > 2) {
-          return charSpan + 1;
+    console.log('charCount start', charCount);
+    if (charCount >= 3) {
+      return 0;
+    } else {
+      console.log(aString);
+      for (let char of aString) {
+        //   if (char !== aChar) {
+        //     return 0;
+        //   }
+        if (char === aChar) {
+          console.log('block', char);
+          const firstCharIndex = aString.indexOf(char);
+          const lastCharIndex = aString.lastIndexOf(char);
+          const charSpan = lastCharIndex - firstCharIndex;
+          console.log('charSpan block', charSpan);
+          if (charSpan === 0) {
+            return 0;
+          } else if (charSpan === 1 || charSpan > 2) {
+            return charSpan + 1;
+          }
         }
       }
     }
+    return;
+  } else {
+    return 0;
   }
-  return;
 };
-console.log(subLength('funny', 'k'));
+console.log(subLength('funny', 'n'));
 
 // subLength('Saturday', 'a'); // returns 6
 // subLength('summer', 'm'); // returns 2
