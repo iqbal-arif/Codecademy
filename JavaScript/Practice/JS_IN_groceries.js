2e110492f7f8229efcc1385c2f984e0ff60f02f2
Arif Iqbal <arif_iqbal@yahoo.com>
2023-12-19 00:03:02 -0600

    modified:   JavaScript/Practice/JS_IN_groceries.js

--- JavaScript/Practice/JS_IN_groceries.js
+++ JavaScript/Practice/JS_IN_groceries.js
@@ -22,7 +22,9 @@ const groceries = (groceryList) => {
   for (let i = itemList.length - 1; i >= 0; i--) {
     console.log(i);
     if (i === 3) {
-      return itemList[i];
+      //   return itemList[i];
+      const itemComma = itemList.includes[i];
+      return itemComma;
     }
   }
 };
