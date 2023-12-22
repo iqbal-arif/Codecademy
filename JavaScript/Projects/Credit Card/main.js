082ef62189b5fb62a7087c77b9e6d01bf80e100b
Arif Iqbal <arif_iqbal@yahoo.com>
2023-12-22 16:40:44 -0600

    new file:   JavaScript/Practice/creditcard.length.docx new file:   JavaScript/Practice/creditcard.length.pdf modified:   JavaScript/Projects/Credit Card/main.js

--- JavaScript/Projects/Credit Card/main.js
+++ JavaScript/Projects/Credit Card/main.js
@@ -4,7 +4,7 @@ const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
 const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
 const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
 const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
-
+//valid1,valid3,invalid1,
 // All invalid credit card numbers
 const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
 const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
@@ -82,5 +82,6 @@ const validateCred = (cardNum) => {
 };
 // console.log(validateCred(valid1));
 
-const findInvalidCards = validateCred(invalid1);
+const findInvalidCards = validateCred(mystery1);
+
 console.log(findInvalidCards);
