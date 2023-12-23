af4ed11a73a485933ad9031d37d4f1148941436f
Arif Iqbal <arif_iqbal@yahoo.com>
2023-12-22 18:12:55 -0600

    new file:   JavaScript/Practice/~$editcard.length.docx modified:   JavaScript/Projects/Credit Card/main.js

--- JavaScript/Projects/Credit Card/main.js
+++ JavaScript/Projects/Credit Card/main.js
@@ -4,7 +4,7 @@ const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
 const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
 const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
 const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
-//valid1,valid3,invalid1,
+//valid1,valid3,invalid1,mystery3
 // All invalid credit card numbers
 const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
 const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
@@ -42,19 +42,23 @@ const batch = [
 // const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
 //
 const validateCred = (cardNum) => {
-  console.log(cardNum);
+  console.log('ori-card', cardNum);
   let evenIndexNum = [];
-  for (let e = 1; e < cardNum.length + 1; e += 2) {
+  for (let e = 1; e < cardNum.length; e += 2) {
     // cardNum[e - 1] === -1 ? evenIndexNum.push(cardNum[e]) : 0;
     let evenIndex = cardNum[e];
     evenIndexNum.push(evenIndex);
   }
-  console.log(evenIndexNum);
+  console.log('evenindex', evenIndexNum);
   //   console.log(evenIndex);
   let doubleDigit = [];
-  for (let i = cardNum.length - 1; i >= 0; i -= 2) {
+  for (let i = cardNum.length; i >= 0; i -= 2) {
     // console.log('Array Length', cardNum.length - 1);
     let otherIndex = i - 1;
+    console.log('othercard', cardNum[otherIndex]);
+    if (cardNum[otherIndex] === undefined) {
+      return;
+    }
     let otherDigit = cardNum[otherIndex] * 2;
     // console.log(otherIndex, cardNum[otherIndex]);
     if (otherDigit > 9) {
@@ -82,6 +86,6 @@ const validateCred = (cardNum) => {
 };
 // console.log(validateCred(valid1));
 
-const findInvalidCards = validateCred(mystery1);
+const findInvalidCards = validateCred(invalid1);
 
 console.log(findInvalidCards);
