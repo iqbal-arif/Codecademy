92403d8074a0f755e59bac14e00213318054972f
Arif Iqbal <arif_iqbal@yahoo.com>
2024-01-03 23:41:55 -0600

    modified:   JavaScript/Projects/Credit Card/main.js

--- JavaScript/Projects/Credit Card/main.js
+++ JavaScript/Projects/Credit Card/main.js
@@ -67,12 +67,12 @@ const validateCred = (cardNum) => {
     // }
     // return 'doubleDigit', doubleDigit;
     let oddIndex = cardNum[e];
-    cardNum[e - 1] === -1 ? evenIndexNum.push(cardNum[e]) : 0;
+    // cardNum[e - 1] === -1 ? evenIndexNum.push(cardNum[e]) : 0;
     // console.log(e);
-    // if (e % 2 == 0) {
-    //   let evenIndex = cardNum[e];
-    //   evenIndexedNum.push(evenIndex);
-    // }
+    if (e % 2 == 0) {
+      let evenIndex = cardNum[e];
+      evenIndexedNum.push(evenIndex);
+    }
     e % 2 === 0 ? evenIndexedNum.push(evenIndex) : oddIndexedNum.push(oddIndex);
   }
   console.log('evenindex', evenIndexedNum, 'oddindex', oddIndexedNum);
