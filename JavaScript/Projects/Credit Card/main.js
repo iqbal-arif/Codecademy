140873f0f264bcb1bff056da6e450465fd291ba6
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2024-03-13 22:50:38 -0600

    Update main.js

--- JavaScript/Projects/Credit Card/main.js
+++ JavaScript/Projects/Credit Card/main.js
@@ -153,8 +153,8 @@ const findInvalidCards = validateCred(valid1);
 
 console.log(findInvalidCards);
 console.log;
-/*
-/*
+// /*
+
 const validateCred = (cardNum) => {
   console.log('ori-card', cardNum.length, cardNum);
   const itemKey = Object.keys(cardNum);
@@ -176,4 +176,4 @@ const validateCred = (cardNum) => {
     // console.log(itemIndex);
   });
 };
-*/
+
