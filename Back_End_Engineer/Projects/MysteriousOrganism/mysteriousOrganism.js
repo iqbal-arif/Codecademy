6d1f7b49f5b61366aadd17861df1def2391046f9
Arif Iqbal <arif_iqbal@yahoo.com>
2024-03-25 22:59:34 -0600

    commented

--- Back_End_Engineer/Projects/MysteriousOrganism/mysteriousOrganism.js
+++ Back_End_Engineer/Projects/MysteriousOrganism/mysteriousOrganism.js
@@ -18,18 +18,19 @@ const returnRandBase = () => {
 //Factory Funcion
 
 const pAequorFactory = (orgNumber, newStrand) =>{
+   /* 
+    uniqueSpecimenNum(orgNumber) {
+        //Verify that newName is a non-empty string before setting as name property
+        if (typeof newName === 'string' && newName.length > 0){
+            this._name = newName; 
+        } else {
+            console.log("ERROR: name must be a non-empty string"); 
+        }
+    };
     return {
             specimenNum : orgNumber,
             dna : newStrand
     };
-
-    set uniqueSpecimenNum(orgNumber){
-        //Verify that newName is a non-empty string before setting as name property
-        if (typeof newName === 'string' && newName.length > 0){
-          this._name = newName; 
-        } else {
-          console.log("ERROR: name must be a non-empty string"); 
-        }
-      }
 };
 
+*/
