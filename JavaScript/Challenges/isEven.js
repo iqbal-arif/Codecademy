e10955692d9339415296ca2e65094b97962657c1
Arif Iqbal <arif_iqbal@yahoo.com>
2024-05-03 23:00:40 -0500

    JavaScript/Challenges/isEven.js

---
+++ JavaScript/Challenges/isEven.js
@@ -0,0 +1,10 @@
+/* Is Even
+Create a function isEven() that takes a number as its only parameter. The function should return true if the number is even and false if the number is odd.
+*/
+// Write function below
+
+const isEven = (num) => {
+  return Number.isInteger(num / 2) ? true : false;
+};
+
+console.log(isEven(206));
