ec67d0367f6dcc739a9e64355d5ddc6baf3711f8
Arif Iqbal <arif_iqbal@yahoo.com>
2024-05-04 22:44:54 -0500

    numberDigits.js added

---
+++ JavaScript/Challenges/numberDigits.js
@@ -0,0 +1,41 @@
+/*
+Create a function numberDigits() that takes the variable x as its only parameter.
+
+If the variable x is between 0 and 9, return the string 'One digit: N', where N is the value of x. For example, numberDigits(5) would return:
+
+'One digit: 5'
+
+If the variable x is between 10 and 99, return the string 'Two digits: N', where N is the value of x. For example, numberDigits(12) would output:
+
+'Two digits: 12'
+
+Any other value of x, including negative numbers, return the string 'The number is: N', where N is the value of x. For example, numberDigits(-202) would output:
+
+'The number is: -202'
+*/
+
+const numberDigits = (num) => {
+  const digitCount = num.toString();
+
+  const numIncludes = digitCount.includes('-');
+
+  if (numIncludes === true) {
+    return `The number is: ${num}`;
+  } else if (digitCount.length === 1) {
+    return `One digit: ${num}`;
+  } else if (digitCount.length === 2) {
+    return `Two digits: ${num}`;
+  } else if (digitCount.length === 3) {
+    return `The number is: ${num}`;
+  }
+};
+
+y = -235;
+
+newNum = y.toString();
+
+const r = newNum.includes('-');
+
+console.log(r);
+
+console.log(numberDigits(100));
