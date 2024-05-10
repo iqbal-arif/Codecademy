4bd35543a7a3d2f667e7f9290d177fdeda760a00
Arif Iqbal <arif_iqbal@yahoo.com>
2024-05-10 15:38:53 -0500

    Added Mysterious Organism project

---
+++ JavaScript/Projects/Credit Card/mysteriousOrganism.js
@@ -0,0 +1,36 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+    const dnaBases = ['A', 'T', 'C', 'G']
+    return dnaBases[Math.floor(Math.random() * 4)] 
+  }
+  
+  // Returns a random single strand of DNA containing 15 bases
+  const mockUpStrand = () => {
+    const newStrand = []
+    for (let i = 0; i < 15; i++) {
+      newStrand.push(returnRandBase())
+    }
+    return newStrand
+  }
+  
+  const pAequorFactory = (orgNumber, newStrand) =>{
+      
+    set  uniqueSpecimenNum(orgNumber) {
+          //Verify that newName is a non-empty string before setting as name property
+          if (typeof newName === 'string' && newName.length > 0){
+              this._name = newName; 
+          } else {
+              console.log("ERROR: name must be a non-empty string"); 
+          }
+      };
+      return {
+              specimenNum : orgNumber,
+              dna : newStrand
+      };
+  };
+  
+  
+  
+  
+  
+  
